80ec66d4dce94785a99e55653366d40aab9d87d6
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-05-11 16:49:48 +0300

    backup: init

---
+++ Backuper/src/Backuper.Server/Controllers/StateController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backuper.Server.Controllers
+{
+    [Route("/")]
+    public class StateController : ControllerBase
+    {
+        public StateController()
+        {
+
+        }
+    }
+}
