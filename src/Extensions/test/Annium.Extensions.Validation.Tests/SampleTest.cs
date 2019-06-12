43b77c274de95fe8d758b8b429c96a397430af96
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-06-12 14:29:57 +0300

    init Annium.Extensions.Validation

---
+++ src/Extensions/test/Annium.Extensions.Validation.Tests/SampleTest.cs
@@ -0,0 +1,17 @@
+using Annium.Testing;
+
+namespace Annium.Extensions.Validation.Tests
+{
+    public class SampleTest
+    {
+        [Fact]
+        public void True_IsTrue()
+        {
+            // arrange
+            var value = true;
+
+            // assert
+            value.IsTrue();
+        }
+    }
+}
