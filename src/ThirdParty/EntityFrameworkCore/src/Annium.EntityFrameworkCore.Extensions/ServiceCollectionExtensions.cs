df0e7694842e5d8af054d234d8c813d7b8cb5756
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-07-13 16:30:51 +0300

    Annium.EntityFrameworkCore.Extensions - allow query logging for InMemory Sqlite configuration

--- src/ThirdParty/EntityFrameworkCore/src/Annium.EntityFrameworkCore.Extensions/ServiceCollectionExtensions.cs
+++ src/ThirdParty/EntityFrameworkCore/src/Annium.EntityFrameworkCore.Extensions/ServiceCollectionExtensions.cs
@@ -2,23 +2,46 @@ using System;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Annium.Extensions.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
-        public static void AddEntityFrameworkSqliteInMemory<TContext>(this IServiceCollection services) where TContext : DbContext
+        public static void AddEntityFrameworkSqliteInMemory<TContext>(
+            this IServiceCollection services,
+            bool logQueries = false
+        )
+        where TContext : DbContext
         {
             var cn = new SqliteConnection("Data Source=:memory:");
             cn.Open();
+
+            var loggerFactory = logQueries ? CreateLoggerFactory() : null;
+
             // register context itself
             services
                 .AddEntityFrameworkSqlite()
                 .AddDbContext<TContext>(builder =>
                 {
+                    if (logQueries)
+                        builder.UseLoggerFactory(loggerFactory);
+
                     var opts = builder.UseSqlite(cn).Options;
                     using(var ctx = Activator.CreateInstance(typeof(TContext), opts) as DbContext) ctx.Database.EnsureCreated();
                 });
+        }
+
+        private static ILoggerFactory CreateLoggerFactory()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(builder =>
+                builder
+                .AddConsole()
+                .AddFilter(DbLoggerCategory.Database.Command.Name, LogLevel.Information));
+
+            return serviceCollection.BuildServiceProvider()
+                .GetService<ILoggerFactory>();
         }
     }
 }
