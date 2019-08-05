2a758be2b13e2bbe0a522b1f317ce8c43a574fb9
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-08-05 23:22:36 +0300

    Annium.Configuration.Abstractions: implement fluent deep configuration

---
+++ src/Configuration/src/Annium.Configuration.Abstractions/ServiceCollectionExtensions.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Linq;
+using Annium.Configuration.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Annium.Core.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddConfiguration<T>(
+            this IServiceCollection services,
+            Action<IConfigurationBuilder> configure
+        )
+        where T : class, new()
+        {
+            var builder = new ConfigurationBuilder();
+            configure(builder);
+            var configuration = builder.Build<T>();
+
+            Register(services, configuration);
+
+            return services;
+        }
+
+        private static void Register(IServiceCollection services, object cfg)
+        {
+            services.AddSingleton(cfg);
+
+            var props = cfg.GetType().GetProperties()
+                .Where(p => p.CanRead && !p.PropertyType.IsEnum && !p.PropertyType.IsValueType && !p.PropertyType.IsPrimitive)
+                .ToList();
+
+            foreach (var prop in props)
+                Register(services, prop.GetValue(cfg));
+        }
+    }
+}
