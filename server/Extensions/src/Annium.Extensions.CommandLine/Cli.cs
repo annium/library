52e7deecb8f9ed842aa9bb0dd149c04fc6c2f94a
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-09-19 00:58:25 +0300

    Annium.Extensions.Commandline: add helpers

--- server/Extensions/src/Annium.Extensions.CommandLine/Cli.cs
+++ server/Extensions/src/Annium.Extensions.CommandLine/Cli.cs
@@ -6,6 +6,32 @@ namespace Annium.Extensions.CommandLine
 {
     public static class Cli
     {
+        public static bool Confirm(string label, bool? defaultValue = null)
+        {
+            var y = defaultValue.HasValue && defaultValue.Value ? 'Y' : 'y';
+            var n = defaultValue.HasValue && !defaultValue.Value ? 'N' : 'n';
+
+            Console.WriteLine($"{label} ({y}/{n})");
+
+            while (true)
+            {
+                var key = Console.ReadKey(intercept: true);
+                if (key.Key == ConsoleKey.Y)
+                    return true;
+                if (key.Key == ConsoleKey.N)
+                    return false;
+                if (defaultValue.HasValue && key.Key == ConsoleKey.Enter)
+                    return defaultValue.Value;
+            }
+        }
+
+        public static string Prompt(string label)
+        {
+            Console.Write(label);
+
+            return Console.ReadLine();
+        }
+
         public static string ReadSecure(string label)
         {
             Console.Write(label);
