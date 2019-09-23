c6e5f56af0031b88e1b7776cef25c6d53a1e368a
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-09-23 10:25:35 +0300

    Annium.Net.Http: Add BasicAuthorization extension

--- server/Net/src/Annium.Net.Http/Extensions/HeaderExtensions.cs
+++ server/Net/src/Annium.Net.Http/Extensions/HeaderExtensions.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Net.Http.Headers;
+using System.Text;
 
 namespace Annium.Net.Http
 {
     public static class HeaderExtensions
     {
+        public static IRequest BasicAuthorization(this IRequest request, string user, string pass) =>
+            request.Authorization(new AuthenticationHeaderValue("Basic", $"{Convert.ToBase64String(Encoding.UTF8.GetBytes($"{user}:{pass}"))}"));
+
         public static IRequest BearerAuthorization(this IRequest request, string token) =>
             request.Authorization(new AuthenticationHeaderValue("Bearer", token));
     }
