b8183c247a3cf6cfb93aeaac51e2aeb2155f743d
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-10-22 22:44:22 +0300

    Net.WebSockets: make IClientWebSocket implement ISendingWebSocket, IReceivingWebSocket

--- server/Net/src/Annium.Net.WebSockets/IClientWebSocket.cs
+++ server/Net/src/Annium.Net.WebSockets/IClientWebSocket.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace Annium.Net.WebSockets
 {
-    public interface IClientWebSocket : IDisposable
+    public interface IClientWebSocket : ISendingWebSocket, IReceivingWebSocket, IDisposable
     {
         Task ConnectAsync(Uri uri, CancellationToken token);
         Task DisconnectAsync(CancellationToken token);
