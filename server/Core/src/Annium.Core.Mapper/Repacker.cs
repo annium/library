f1ddadbc46d0881ac5243d4351654137154964e1
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-10-22 23:58:17 +0300

    Core.Mapper: update Repacker

--- server/Core/src/Annium.Core.Mapper/Repacker.cs
+++ server/Core/src/Annium.Core.Mapper/Repacker.cs
@@ -22,32 +22,36 @@ namespace Annium.Core.Mapper
                 MethodCallExpression call => Call(call)(source),
                 NewExpression construction => New(construction)(source),
                 ParameterExpression param => source,
+                UnaryExpression unary => Unary(unary)(source),
                 _ => throw new InvalidOperationException($"Can't repack {ex.NodeType} expression"),
             };
         };
 
         private Func<Expression, Expression> Lambda(LambdaExpression ex) => (Expression source) =>
-            Expression.Lambda(Repack(ex.Body) (source), new [] { source as ParameterExpression });
+            Expression.Lambda(Repack(ex.Body)(source), new[] { source as ParameterExpression });
 
         private Func<Expression, Expression> Member(MemberExpression ex) => (Expression source) =>
-            Expression.MakeMemberAccess(Repack(ex.Expression) (source), ex.Member);
+            Expression.MakeMemberAccess(Repack(ex.Expression)(source), ex.Member);
 
         private Func<Expression, Expression> MemberInit(MemberInitExpression ex) => (Expression source) =>
             Expression.MemberInit(
-                Repack(ex.NewExpression) (source) as NewExpression,
+                Repack(ex.NewExpression)(source) as NewExpression,
                 ex.Bindings.Select(b =>
                 {
                     if (b is MemberAssignment ma)
-                        return ma.Update(Repack(ma.Expression) (source));
+                        return ma.Update(Repack(ma.Expression)(source));
 
                     return b;
                 })
             );
 
         private Func<Expression, Expression> Call(MethodCallExpression ex) => (Expression source) =>
-            Expression.Call(Repack(ex.Object) (source), ex.Method, ex.Arguments.Select(a => Repack(a) (source)).ToArray());
+            Expression.Call(Repack(ex.Object)(source), ex.Method, ex.Arguments.Select(a => Repack(a)(source)).ToArray());
 
         private Func<Expression, Expression> New(NewExpression ex) => (Expression source) =>
-            Expression.New(ex.Constructor, ex.Arguments.Select(a => Repack(a) (source)));
+            Expression.New(ex.Constructor, ex.Arguments.Select(a => Repack(a)(source)));
+
+        private Func<Expression, Expression> Unary(UnaryExpression ex) => (Expression source) =>
+            Expression.MakeUnary(ex.NodeType, Repack(ex.Operand)(source), ex.Type, ex.Method);
     }
 }
