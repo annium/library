e470d7634b3aa891de4eae7e59a5775726d3ebca
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-11-30 00:23:52 +0300

    Net.Http: rename HttpContentExtensions

--- server/Net/src/Annium.Net.Http/Internal/HttpContentExtensions.cs
+++ server/Net/src/Annium.Net.Http/Internal/HttpContentExtensions.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Annium.Net.Http.Internal
 {
-    internal static class Content
+    internal static class HttpContentExtensions
     {
         public static async Task<T> ParseAsync<T>(this HttpContent content)
         {
