bb304e0e6743b85f675cda0e6d6ed663e5e829cc
Alex Kreskiyan <a.kreskiyan@gmail.com>
2019-12-29 16:20:38 +0300

    Enable JsonStringEnumConverter by default

--- third-party/AspNetCore/src/Annium.AspNetCore.Extensions/ConfigurationExtensions.cs
+++ third-party/AspNetCore/src/Annium.AspNetCore.Extensions/ConfigurationExtensions.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using NodaTime;
 
@@ -7,16 +10,23 @@ namespace Annium.Core.DependencyInjection
     public static class ConfigurationExtensions
     {
         public static IMvcBuilder AddDefaultJsonOptions(
-            this IMvcBuilder builder
+            this IMvcBuilder builder,
+            Action<JsonOptions> configure
         ) => builder.AddJsonOptions(opts =>
         {
             opts.JsonSerializerOptions
-            .ConfigureAbstractConverter()
-            .ConfigureForOperations()
-            .ConfigureForNodaTime(DateTimeZoneProviders.Serialization);
+                .ConfigureAbstractConverter()
+                .ConfigureForOperations()
+                .ConfigureForNodaTime(DateTimeZoneProviders.Serialization);
+            opts.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
             opts.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
             opts.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
             opts.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+
+            configure(opts);
         });
+
+        public static IMvcBuilder AddDefaultJsonOptions(this IMvcBuilder builder) =>
+            builder.AddDefaultJsonOptions(opts => { });
     }
 }
