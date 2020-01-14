cfed2bea197d89e029626303cc03e15d7d13de7d
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-01-14 21:46:28 +0300

    Testing: delete assetions

--- server/Testing/src/Annium.Testing/AssertionFailedException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Annium.Testing
-{
-    public class AssertionFailedException : Exception
-    {
-        public AssertionFailedException(string message) : base(message) { }
-    }
-}
