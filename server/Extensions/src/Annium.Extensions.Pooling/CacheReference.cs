c5d402b9209250e1ce92cd809c86823ddf7006a3
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-01-24 10:46:09 +0300

    Extensions.Pooling: add static CacheReference.Create factory

--- server/Extensions/src/Annium.Extensions.Pooling/CacheReference.cs
+++ server/Extensions/src/Annium.Extensions.Pooling/CacheReference.cs
@@ -3,6 +3,21 @@ using System.Threading.Tasks;
 
 namespace Annium.Extensions.Pooling
 {
+    public static class CacheReference
+    {
+        public static ICacheReference<TValue> Create<TValue>(TValue value)
+            where TValue : notnull
+        {
+            return new CacheReference<TValue>(value, () => Task.CompletedTask);
+        }
+
+        public static ICacheReference<TValue> Create<TValue>(TValue value, Func<Task> dispose)
+            where TValue : notnull
+        {
+            return new CacheReference<TValue>(value, dispose);
+        }
+    }
+
     internal class CacheReference<TValue> : ICacheReference<TValue>
         where TValue : notnull
     {
