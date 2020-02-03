e235eaf7df6ab678e2c049b29308336898f0bf98
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-02-04 02:08:19 +0300

    Extensions.Primitives: add AsyncLazy concurrent tests

--- server/Extensions/test/Annium.Extensions.Primitives.Tests/AsyncLazyTest.cs
+++ server/Extensions/test/Annium.Extensions.Primitives.Tests/AsyncLazyTest.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using System.Threading.Tasks;
 using Annium.Testing;
 using Xunit;
@@ -20,16 +21,76 @@ namespace Annium.Extensions.Primitives.Tests
         }
 
         [Fact]
+        public async Task SyncFactory_Concurrent_Works()
+        {
+            // arrange
+            var counter = 0;
+            var lazy = new AsyncLazy<int>(() => Interlocked.Increment(ref counter));
+
+            // act
+            var values = await Task.WhenAll(
+                Task.Run(async () => await lazy),
+                Task.Run(async () => await lazy),
+                Task.Run(async () => await lazy),
+                Task.Run(async () => await lazy),
+                Task.Run(async () => await lazy),
+                Task.Run(async () => await lazy),
+                Task.Run(async () => await lazy),
+                Task.Run(async () => await lazy),
+                Task.Run(async () => await lazy),
+                Task.Run(async () => await lazy)
+            );
+
+            // assert
+            values.IsEqual(new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
+            counter.IsEqual(1);
+        }
+
+        [Fact]
         public async Task AsyncFactory_Works()
         {
             // arrange
-            var lazy = new AsyncLazy<int>(async () => { await Task.Delay(5); return 10; });
+            var lazy = new AsyncLazy<int>(async () =>
+            {
+                await Task.Delay(5);
+                return 10;
+            });
 
             // act
             var value = await lazy;
 
             // assert
             value.IsEqual(10);
+        }
+
+        [Fact]
+        public async Task AsyncFactory_Concurrent_Works()
+        {
+            // arrange
+            var counter = 0;
+            var lazy = new AsyncLazy<int>(async () =>
+            {
+                await Task.Delay(5);
+                return Interlocked.Increment(ref counter);
+            });
+
+            // act
+            var values = await Task.WhenAll(
+                Task.Run(async () => await lazy),
+                Task.Run(async () => await lazy),
+                Task.Run(async () => await lazy),
+                Task.Run(async () => await lazy),
+                Task.Run(async () => await lazy),
+                Task.Run(async () => await lazy),
+                Task.Run(async () => await lazy),
+                Task.Run(async () => await lazy),
+                Task.Run(async () => await lazy),
+                Task.Run(async () => await lazy)
+            );
+
+            // assert
+            values.IsEqual(new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
+            counter.IsEqual(1);
         }
     }
 }
