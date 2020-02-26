9e068d24b76aaba04e5a18d37b6a580c1c2c5efd
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-02-26 10:26:26 +0300

    Update deps

--- third-party/AspNetCore/test/Demo.AspNetCore/Program.cs
+++ third-party/AspNetCore/test/Demo.AspNetCore/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace Demo.AspNetCore
 {
-    internal class Program
+    internal static class Program
     {
         public static void Main(string[] args)
         {
