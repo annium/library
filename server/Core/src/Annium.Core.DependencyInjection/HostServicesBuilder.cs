5a36ef6b480ad4fff754af362054a3eaa09b171d
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-03-07 10:27:03 +0300

    Core.DependencyInjection: add HostServicesBuilder

---
+++ server/Core/src/Annium.Core.DependencyInjection/HostServicesBuilder.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Annium.Core.DependencyInjection
+{
+    /// <summary>
+    /// This is emulation class for compatibility with extensions, expection HostBuilder pattern implementation
+    /// </summary>
+    /// <typeparam name="TServicePack"></typeparam>
+    public class HostServicesBuilder<TServicePack>
+        where TServicePack : ServicePackBase, new()
+    {
+        public HostServicesProvider Build()
+        {
+            ServiceProviderBuilder builder = new ServiceProviderBuilder();
+            builder.UseServicePack<TServicePack>();
+
+            return new HostServicesProvider(builder.Build());
+        }
+    }
+
+    public class HostServicesProvider
+    {
+        public IServiceProvider Services { get; }
+
+        public HostServicesProvider(IServiceProvider services)
+        {
+            Services = services;
+        }
+    }
+}
