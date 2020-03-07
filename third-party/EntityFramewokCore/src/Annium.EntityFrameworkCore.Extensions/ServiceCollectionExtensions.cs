67b9c12e3f4acb0f8a327992c824ee80ec1a33bd
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-03-07 10:46:57 +0300

    EntityFrameworkCore.Extensions: cleanup

--- third-party/EntityFramewokCore/src/Annium.EntityFrameworkCore.Extensions/ServiceCollectionExtensions.cs
+++ third-party/EntityFramewokCore/src/Annium.EntityFrameworkCore.Extensions/ServiceCollectionExtensions.cs
@@ -2,7 +2,6 @@ using System;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace Annium.Core.DependencyInjection
 {
@@ -12,37 +11,20 @@ namespace Annium.Core.DependencyInjection
             this IServiceCollection services,
             bool logQueries = false
         )
-        where TContext : DbContext
+            where TContext : DbContext
         {
             var cn = new SqliteConnection("Data Source=:memory:");
             cn.Open();
-
-            var loggerFactory = logQueries ? CreateLoggerFactory() : null;
 
             // register context itself
             services
                 .AddEntityFrameworkSqlite()
                 .AddDbContext<TContext>(builder =>
                 {
-                    if (logQueries && loggerFactory != null)
-                        builder.UseLoggerFactory(loggerFactory);
-
                     var opts = builder.UseSqlite(cn).Options;
-                    using var ctx = (Activator.CreateInstance(typeof(TContext), opts) as DbContext) !;
+                    using var ctx = Activator.CreateInstance(typeof(TContext), opts) as DbContext;
                     ctx.Database.EnsureCreated();
                 });
-        }
-
-        private static ILoggerFactory CreateLoggerFactory()
-        {
-            var serviceCollection = new ServiceCollection();
-            serviceCollection.AddLogging(builder =>
-                builder
-                .AddConsole()
-                .AddFilter(DbLoggerCategory.Database.Command.Name, LogLevel.Information));
-
-            return serviceCollection.BuildServiceProvider()
-                .GetService<ILoggerFactory>();
         }
     }
 }
