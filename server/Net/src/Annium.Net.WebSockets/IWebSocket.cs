5e233e624b78ed9e1b13fc5e4356c0da5e689078
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-03-29 16:09:19 +0300

    Net.WebSockets: add WebSocket wrapper

---
+++ server/Net/src/Annium.Net.WebSockets/IWebSocket.cs
@@ -0,0 +1,10 @@
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Annium.Net.WebSockets
+{
+    public interface IWebSocket : ISendingReceivingWebSocket
+    {
+        Task DisconnectAsync(CancellationToken token);
+    }
+}
---
+++ server/Net/src/Annium.Net.WebSockets/WebSocket.cs
@@ -0,0 +1,26 @@
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Annium.Serialization.Abstractions;
+using NativeWebSocket = System.Net.WebSockets.WebSocket;
+
+namespace Annium.Net.WebSockets
+{
+    public class WebSocket : WebSocketBase<NativeWebSocket>, IWebSocket
+    {
+        public WebSocket(
+            NativeWebSocket socket,
+            ISerializer<byte[]> serializer
+        ) : base(
+            socket,
+            serializer
+        )
+        {
+        }
+
+        public async Task DisconnectAsync(CancellationToken token)
+        {
+            await Socket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, token);
+        }
+    }
+}
