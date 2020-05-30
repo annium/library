4ef5b127fb7966089790d15955f63ba1e87516a8
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-05-30 12:17:47 +0300

    Extensions.Validation: cleanup AddValidation

--- server/Extensions/src/Annium.Extensions.Validation/ServiceCollectionExtensions.cs
+++ server/Extensions/src/Annium.Extensions.Validation/ServiceCollectionExtensions.cs
@@ -1,5 +1,3 @@
-using System.Linq;
-using Annium.Core.Runtime.Types;
 using Annium.Extensions.Validation;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,18 +7,11 @@ namespace Annium.Core.DependencyInjection
     {
         public static IServiceCollection AddValidation(this IServiceCollection services)
         {
-            var typeManager = TypeManager.Instance;
-
-            var validatorBase = typeof(IValidationContainer<>);
-            var validatorTypes = typeManager.GetImplementations(validatorBase);
-            foreach (var type in validatorTypes)
-            {
-                var baseType = type.GetInterfaces()
-                    .First(i => i.IsGenericType && i.GetGenericTypeDefinition() == validatorBase);
-
-                if (!baseType.ContainsGenericParameters)
-                    services.AddScoped(baseType, type);
-            }
+            services.AddAllTypes()
+                .AssignableTo(typeof(Validator<>))
+                .Where(x => !x.IsGenericType)
+                .AsImplementedInterfaces()
+                .InstancePerScope();
 
             services.AddScoped(typeof(IValidator<>), typeof(ValidationExecutor<>));
 
