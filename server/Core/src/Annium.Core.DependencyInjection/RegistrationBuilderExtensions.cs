5e7cc0f1c79d3f0fd888821b5626c7144e0331f6
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-05-30 12:16:52 +0300

    Core.DependencyInjection: fix AssignableTo extension to use IsDerivedFrom

--- server/Core/src/Annium.Core.DependencyInjection/RegistrationBuilderExtensions.cs
+++ server/Core/src/Annium.Core.DependencyInjection/RegistrationBuilderExtensions.cs
@@ -1,14 +1,15 @@
 using System;
+using Annium.Core.Reflection;
 
 namespace Annium.Core.DependencyInjection
 {
     public static class RegistrationBuilderExtensions
     {
         public static IRegistrationBuilder AssignableTo<T>(this IRegistrationBuilder builder)
-            => builder.Where(typeof(T).IsAssignableFrom);
+            => builder.Where(x => x.IsDerivedFrom(typeof(T)));
 
         public static IRegistrationBuilder AssignableTo(this IRegistrationBuilder builder, Type baseType)
-            => builder.Where(baseType.IsAssignableFrom);
+            => builder.Where(x => x.IsDerivedFrom(baseType));
 
         public static IRegistrationBuilder StartingWith(this IRegistrationBuilder builder, string prefix)
             => builder.Where(x => x.Name.StartsWith(prefix));
