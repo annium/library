637039570dbfbc7ff8ae01e1f35bbfa7d6369a4e
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-07-29 21:08:57 +0300

    Core.Runtime: disable parameterless TypeManager.GetInstance

--- server/Core/src/Annium.Core.Runtime/Types/TypeManager.cs
+++ server/Core/src/Annium.Core.Runtime/Types/TypeManager.cs
@@ -8,8 +8,6 @@ namespace Annium.Core.Runtime.Types
         private static readonly ConcurrentDictionary<Assembly, ITypeManager> Instances =
             new ConcurrentDictionary<Assembly, ITypeManager>();
 
-        public static ITypeManager GetInstance() => GetInstance(Assembly.GetEntryAssembly()!);
-
         public static ITypeManager GetInstance(Assembly assembly) =>
             Instances.GetOrAdd(assembly, a => new TypeManagerInstance(a));
     }
