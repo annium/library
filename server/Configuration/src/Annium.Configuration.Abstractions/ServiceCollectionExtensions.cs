5a9d3ca572bef43e5eaf385df04e022e5b529ae0
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-08-01 14:46:55 +0300

    Configuration.Abstractions: add injectable IConfigurationBuilder

--- server/Configuration/src/Annium.Configuration.Abstractions/ServiceCollectionExtensions.cs
+++ server/Configuration/src/Annium.Configuration.Abstractions/ServiceCollectionExtensions.cs
@@ -2,25 +2,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Annium.Configuration.Abstractions;
-using Annium.Core.Mapper;
 using Annium.Core.Reflection;
-using Annium.Core.Runtime.Types;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Annium.Core.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
+        public static IServiceCollection AddConfigurationBuilder(
+            this IServiceCollection services
+        )
+        {
+            services.AddTransient<IConfigurationBuilder, ConfigurationBuilder>();
+
+            return services;
+        }
+
         public static IServiceCollection AddConfiguration<T>(
             this IServiceCollection services,
             Action<IConfigurationBuilder> configure
         )
             where T : class, new()
         {
-            var serviceProvider = services.Clone().AddMapper().BuildServiceProvider();
-            var typeManager = serviceProvider.GetRequiredService<ITypeManager>();
-            var mapper = serviceProvider.GetRequiredService<IMapper>();
-            var builder = new ConfigurationBuilder(typeManager, mapper);
+            var serviceProvider = services.Clone().AddMapper().AddConfigurationBuilder().BuildServiceProvider();
+            var builder = serviceProvider.GetRequiredService<IConfigurationBuilder>();
             configure(builder);
             var configuration = builder.Build<T>();
 
