169ee1ebf129da9b8dc356276931211e1e18675e
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-08-02 15:22:14 +0300

    Extensions.Primitives: add DirectoryInfo & FileInfo extensions

---
+++ server/Extensions/src/Annium.Extensions.Primitives/FileInfoExtensions.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+
+namespace Annium.Extensions.Primitives
+{
+    public static class FileInfoExtensions
+    {
+        public static bool IsAt(this FileInfo value, string root)
+        {
+            return value.IsAt(new DirectoryInfo(root));
+        }
+
+        public static bool IsAt(this FileInfo value, DirectoryInfo root)
+        {
+            if (value is null)
+                throw new ArgumentNullException(nameof(value));
+
+            var rootPath = root.FullName.EndsWith(Path.DirectorySeparatorChar)
+                ? root.FullName
+                : $"{root.FullName}{Path.DirectorySeparatorChar}";
+
+            return value.FullName.Length > rootPath.Length && value.FullName.StartsWith(rootPath);
+        }
+    }
+}
---
+++ server/Extensions/test/Annium.Extensions.Primitives.Tests/FileInfoExtensionsTest.cs
@@ -0,0 +1,22 @@
+using System;
+using System.IO;
+using Annium.Testing;
+using Xunit;
+
+namespace Annium.Extensions.Primitives.Tests
+{
+    public class FileInfoExtensionsTest
+    {
+        [Fact]
+        public void IsAt_WorksCorrectly()
+        {
+            var root = new DirectoryInfo(Directory.GetCurrentDirectory()).Root.FullName;
+            GetFile("x.txt").IsAt(Path.Combine(root, Guid.NewGuid().ToString())).IsFalse();
+            GetFile("x.txt").IsAt(root).IsTrue();
+            GetFile(Path.Combine(root, "xx", "..", "x.txt")).IsAt(root).IsTrue();
+            GetFile(Path.Combine(root, "xx", "..", "x.txt")).IsAt(Path.Combine(root, "xxx")).IsFalse();
+        }
+
+        private FileInfo GetFile(string path) => new FileInfo(path);
+    }
+}
