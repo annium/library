2d474107c7aa43786d4f6a581decabba1d4f7795
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-08-09 21:08:18 +0300

    Blazor.Core: update references

--- client/Blazor/src/Annium.Blazor.Core/Extensions/ComponentBaseStateExtensions.cs
+++ client/Blazor/src/Annium.Blazor.Core/Extensions/ComponentBaseStateExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Annium.Components.Forms;
+using Annium.Components.State;
 using Annium.Core.Reflection;
 using Microsoft.AspNetCore.Components;
 
