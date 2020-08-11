27e7531145a9b7f00ea8a6deba6d3b9944fdb3ed
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-08-11 23:43:44 +0300

    Blazor.Ant: prepare basic input/checkbox demos

---
+++ client/Blazor/src/Annium.Blazor.Ant/Components/Checkbox.razor.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using Annium.Components.State;
+using Microsoft.AspNetCore.Components;
+
+namespace Annium.Blazor.Ant.Components
+{
+    public partial class Checkbox
+    {
+        [Parameter]
+        public IAtomicContainer<bool> State { get; set; } = default!;
+
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IReadOnlyDictionary<string, object> Attributes { get; set; } = default!;
+    }
+}
---
+++ client/Blazor/src/Annium.Blazor.Ant/Components/TextField.razor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Annium.Components.State;
+using Microsoft.AspNetCore.Components;
+
+namespace Annium.Blazor.Ant.Components
+{
+    public partial class TextField<TValue> where TValue : IEquatable<TValue>
+    {
+        [Parameter]
+        public IAtomicContainer<TValue> State { get; set; } = default!;
+
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IReadOnlyDictionary<string, object> Attributes { get; set; } = default!;
+    }
+}
