9e347fc39afaa37ee8f1d1216ed57a583aff9ee4
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-08-12 22:07:27 +0300

    Blazor.MatBlazor: fix components and add backing classes

---
+++ client/Blazor/src/Annium.Blazor.MatBlazor/Components/Checkbox.razor.cs
@@ -0,0 +1,21 @@
+using Annium.Components.State;
+using Microsoft.AspNetCore.Components;
+
+namespace Annium.Blazor.MatBlazor.Components
+{
+    public partial class Checkbox
+    {
+        [Parameter]
+        public IAtomicContainer<bool> State { get; set; } = default!;
+
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        [Parameter]
+        public string? Class { get; set; }
+
+        [Parameter]
+        public bool Disabled { get; set; }
+
+    }
+}
--- client/Blazor/src/Annium.Blazor.MatBlazor/Components/TextField.razor.cs
+++ client/Blazor/src/Annium.Blazor.MatBlazor/Components/TextField.razor.cs
@@ -1,8 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using Annium.Blazor.Core.Tools;
+using Annium.Components.State;
+using Microsoft.AspNetCore.Components;
 
 namespace Annium.Blazor.MatBlazor.Components
 {
     public partial class TextField<TValue> where TValue : IEquatable<TValue>
     {
+        [Parameter]
+        public IAtomicContainer<TValue> State { get; set; } = default!;
+
+        [Parameter]
+        public string? Class { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IReadOnlyDictionary<string, object> Attributes { get; set; } = default!;
+
+        public string ClassName => _classBuilder.Clone().With(Class ?? string.Empty).Build();
+
+        private readonly ClassBuilder _classBuilder;
+
+        public TextField()
+        {
+            _classBuilder = new ClassBuilder()
+                .With(() => State.IsStatus(Status.Error), "mdc-text-field--invalid");
+        }
     }
 }
