e03c280b559333e009441e7dd8479fc4520ff742
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-08-19 23:30:08 +0300

    Components.State.Core: add ObservableState

---
+++ client/Components/src/Annium.Components.State.Core/IObservableState.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Reactive;
+
+namespace Annium.Components.State.Core
+{
+    public interface IObservableState
+    {
+        IObservable<Unit> Changed { get; }
+        IDisposable Mute();
+    }
+}
---
+++ client/Components/src/Annium.Components.State.Core/ObservableState.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace Annium.Components.State.Core
+{
+    public class ObservableState : IObservableState
+    {
+        public IObservable<Unit> Changed { get; }
+        private event Action StateChanged = () => { };
+        private bool _isMuted;
+
+        protected ObservableState()
+        {
+            Changed = Observable.FromEvent(
+                handle => StateChanged += handle,
+                handle => StateChanged -= handle
+            );
+        }
+
+        public IDisposable Mute()
+        {
+            _isMuted = true;
+
+            return new MuteScope(this);
+        }
+
+        protected void OnChanged()
+        {
+            if (!_isMuted)
+                StateChanged.Invoke();
+        }
+
+        private void Unmute() => _isMuted = false;
+
+        private class MuteScope : IDisposable
+        {
+            private readonly ObservableState _state;
+
+            public MuteScope(
+                ObservableState state
+            )
+            {
+                _state = state;
+            }
+
+            public void Dispose()
+            {
+                _state.Unmute();
+            }
+        }
+    }
+}
