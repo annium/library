b9ea67a48d8f17ad161f1c406d243fdb4d882d0f
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-08-22 20:53:19 +0300

    Core.Runtime: improve GetInstance to cleanup cache wen assembly is garbage collected

--- server/Core/src/Annium.Core.Runtime/Types/TypeManager.cs
+++ server/Core/src/Annium.Core.Runtime/Types/TypeManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Concurrent;
 using System.Reflection;
 using Annium.Core.Runtime.Internal.Types;
@@ -6,10 +7,21 @@ namespace Annium.Core.Runtime.Types
 {
     public static class TypeManager
     {
-        private static readonly ConcurrentDictionary<Assembly, ITypeManager> Instances =
-            new ConcurrentDictionary<Assembly, ITypeManager>();
+        private static readonly ConcurrentDictionary<ITrackingWeakReference<Assembly>, ITypeManager> Instances =
+            new ConcurrentDictionary<ITrackingWeakReference<Assembly>, ITypeManager>();
 
-        public static ITypeManager GetInstance(Assembly assembly) =>
-            Instances.GetOrAdd(assembly, a => new TypeManagerInstance(a));
+        public static ITypeManager GetInstance(Assembly assembly)
+        {
+            var reference = TrackingWeakReference.Get(assembly);
+            reference.Collected += () => Instances.TryRemove(reference, out _);
+
+            return Instances.GetOrAdd(reference, x =>
+            {
+                if (!x.TryGetTarget(out var asm))
+                    throw new ObjectDisposedException("Assembly reference is disposed");
+
+                return new TypeManagerInstance(asm);
+            });
+        }
     }
 }
