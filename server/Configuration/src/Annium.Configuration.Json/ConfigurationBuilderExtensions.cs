d08a4d75b0e3c3748bbf7d671801bd56d2e0a7bd
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-08-25 23:53:27 +0300

    Configuration: add remote Json/Yaml configurations

--- server/Configuration/src/Annium.Configuration.Json/ConfigurationBuilderExtensions.cs
+++ server/Configuration/src/Annium.Configuration.Json/ConfigurationBuilderExtensions.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Net.Http;
 using Annium.Configuration.Json.Internal;
 
 namespace Annium.Configuration.Abstractions
@@ -19,6 +20,27 @@ namespace Annium.Configuration.Abstractions
                     throw new FileNotFoundException($"Json configuration file {path} not found and is not optional");
 
             var raw = File.ReadAllText(path);
+            var configuration = new JsonConfigurationProvider(raw).Read();
+
+            return builder.Add(configuration);
+        }
+
+        public static IConfigurationBuilder AddRemoteJson(
+            this IConfigurationBuilder builder,
+            string uri,
+            bool optional = false
+        )
+        {
+            var client = new HttpClient();
+            var message = new HttpRequestMessage(HttpMethod.Get, uri);
+            var response = client.SendAsync(message).Result;
+            if (!response.IsSuccessStatusCode)
+                if (optional)
+                    return builder;
+                else
+                    throw new FileNotFoundException($"Json configuration not available at {uri} and is not optional");
+
+            var raw = response.Content.ReadAsStringAsync().Result;
             var configuration = new JsonConfigurationProvider(raw).Read();
 
             return builder.Add(configuration);
