ea5340f7f30c6555719d858641741a849fe074ef
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-11-17 02:30:04 +0300

    Core.Internal: add

---
+++ server/Core/src/Annium.Core.Internal/Framework.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace Annium.Core.Internal
+{
+    public static class Framework
+    {
+        private static SetupMode Mode { get; }
+        private const string ModeVar = "ANNIUM_MODE";
+
+        static Framework()
+        {
+            var modeValue = Environment.GetEnvironmentVariable(ModeVar);
+            if (modeValue is not null && Enum.TryParse(modeValue, true, out SetupMode mode))
+                Mode = mode;
+        }
+
+        public static void Log(
+            Func<string> getMessage,
+            [CallerFilePath] string callerFilePath = "",
+            [CallerMemberName] string member = ""
+        )
+        {
+            if (Mode == SetupMode.Debug)
+            {
+                var caller = Path.GetFileNameWithoutExtension(callerFilePath);
+                Console.WriteLine($"{caller}.{member}: {getMessage()}");
+            }
+        }
+    }
+}
---
+++ server/Core/src/Annium.Core.Internal/SetupMode.cs
@@ -0,0 +1,8 @@
+namespace Annium.Core.Internal
+{
+    public enum SetupMode
+    {
+        Debug,
+        Release,
+    }
+}
