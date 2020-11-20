3325f16372871c6fd34d1fda89145489a043def6
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-11-20 03:07:09 +0300

    Core.DependencyInjection: cleanup ServiceContainer extensions

--- server/Core/src/Annium.Core.DependencyInjection/Container/ServiceContainerExtensions.cs
+++ server/Core/src/Annium.Core.DependencyInjection/Container/ServiceContainerExtensions.cs
@@ -6,6 +6,12 @@ namespace Annium.Core.DependencyInjection
 {
     public static class ServiceContainerExtensions
     {
+        public static ISingleRegistrationBuilderBase Add<TImplementationType>(this IServiceContainer container) =>
+            container.Add(typeof(TImplementationType));
+
+        public static ISingleRegistrationBuilderBase TryAdd<TImplementationType>(this IServiceContainer container) =>
+            container.TryAdd(typeof(TImplementationType));
+
         // public static IBulkRegistrationBuilderBase AddAll(this IServiceContainer container)
         //     => container.Add(container.GetTypeManager().Types.AsEnumerable());
         //
