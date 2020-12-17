945be4ce9723b6cdbda44c9aa67c3ea99bf9d5b2
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-12-17 11:35:41 +0300

    Net.Base: add uriQuery

---
+++ server/Net/test/Annium.Net.Base.Tests/UriQueryTest.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using Annium.Testing;
+using Xunit;
+
+namespace Annium.Net.Base.Tests
+{
+    public class UriQueryTest
+    {
+        [Fact]
+        public void Parse_Works()
+        {
+            // act
+            var qn = UriQuery.Parse(null!);
+            var q1 = UriQuery.Parse("");
+            var q2 = UriQuery.Parse("value");
+            var q3 = UriQuery.Parse("a=1&b=2&b=3");
+            var q4 = UriQuery.Parse("a=1&b=2,3");
+
+            // assert
+            qn.ToString().IsEqual("");
+            q1.ToString().IsEqual("");
+            q2.ToString().IsEqual("?value=");
+            q3.ToString().IsEqual("?a=1&b=2&b=3");
+            q4.ToString().IsEqual("?a=1&b=2,3");
+        }
+
+        [Fact]
+        public void Equality_Works()
+        {
+            // act
+            var hashSet = new HashSet<UriQuery>();
+            var q1 = UriQuery.Parse("a=1&b=3");
+            var q2 = UriQuery.Parse("?b=2&a=1&b=3");
+            var q3 = UriQuery.Parse("a=1&b=2&b=3");
+
+            // assert
+            (q1 == q2).IsFalse();
+            (q1 == q3).IsFalse();
+            (q2 == q3).IsTrue();
+            hashSet.Add(q1).IsTrue();
+            hashSet.Add(q2).IsTrue();
+            hashSet.Add(q3).IsFalse();
+        }
+    }
+}
