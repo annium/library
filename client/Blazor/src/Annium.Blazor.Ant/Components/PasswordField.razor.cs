3f2f17fae5cf268312228b731b721eb6cd922eab
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-12-22 01:39:59 +0300

    Blazor.Ant: add PasswordField

---
+++ client/Blazor/src/Annium.Blazor.Ant/Components/PasswordField.razor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Annium.Components.State.Forms;
+using Microsoft.AspNetCore.Components;
+
+namespace Annium.Blazor.Ant.Components
+{
+    public partial class PasswordField
+    {
+        [CascadingParameter]
+        public IFormField<string> FormField { get; set; } = default!;
+
+        [Parameter]
+        public IAtomicContainer<string> State { get; set; } = default!;
+
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IReadOnlyDictionary<string, object> Attributes { get; set; } = default!;
+
+        private string Value => InternalState.Value;
+
+        private void SetValue(ChangeEventArgs args)
+        {
+            try
+            {
+                InternalState.Set(mapper.Map<string>(args.Value!));
+            }
+            catch
+            {
+            }
+        }
+
+        private IAtomicContainer<string> InternalState => State ?? FormField.State;
+    }
+}
