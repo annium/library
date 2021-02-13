1ad9c87f521f87515e647ee7bca38901b5522df7
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-02-14 01:13:13 +0300

    Architecture.Base: make OperationStatus enum

--- server/Architecture/src/Annium.Architecture.Base/OperationStatus.cs
+++ server/Architecture/src/Annium.Architecture.Base/OperationStatus.cs
@@ -1,45 +1,12 @@
-using System;
-using System.Collections.Generic;
-using Annium.Data.Models;
-
 namespace Annium.Architecture.Base
 {
-    public class OperationStatus : Equatable<OperationStatus>
+    public enum OperationStatus
     {
-        private static readonly IDictionary<string, OperationStatus> Statuses = new Dictionary<string, OperationStatus>();
-
-        public static OperationStatus BadRequest { get; } = Register(nameof(BadRequest));
-        public static OperationStatus Conflict { get; } = Register(nameof(Conflict));
-        public static OperationStatus Forbidden { get; } = Register(nameof(Forbidden));
-        public static OperationStatus NotFound { get; } = Register(nameof(NotFound));
-        public static OperationStatus Ok { get; } = Register(nameof(Ok));
-        public static OperationStatus UncaughtException { get; } = Register(nameof(UncaughtException));
-
-        public static OperationStatus Register(string name)
-        {
-            if (Statuses.TryGetValue(name, out var status))
-                return status;
-
-            return Statuses[name] = new OperationStatus(name);
-        }
-
-        public static OperationStatus Get(string name)
-        {
-            if (!Statuses.TryGetValue(name, out var status))
-                throw new Exception($"Operation status {name} is not registered.");
-
-            return status;
-        }
-
-        private readonly string _name;
-
-        private OperationStatus(string name)
-        {
-            _name = name;
-        }
-
-        public override int GetHashCode() => _name.GetHashCode();
-
-        public override string ToString() => _name;
+        BadRequest,
+        Conflict,
+        Forbidden,
+        NotFound,
+        Ok,
+        UncaughtException,
     }
 }
