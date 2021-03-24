31c96ce732c25ba1a5930379963ad4cf785a02e2
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-03-25 00:00:00 +0300

    Data.Tables: add more extensions

---
+++ server/Data/src/Annium.Data.Tables/ChangeEventExtensions.cs
@@ -0,0 +1,17 @@
+using System;
+using Annium.Core.Mapper;
+
+namespace Annium.Data.Tables
+{
+    public static class ChangeEventExtensions
+    {
+        public static IChangeEvent<TOut> MapChangeEvent<TIn, TOut>(this IMapper mapper, IChangeEvent<TIn> e) => e switch
+        {
+            InitEvent<TIn> x => ChangeEvent.Init(mapper.Map<TOut[]>(x.Values)),
+            AddEvent<TIn> x => ChangeEvent.Add(mapper.Map<TOut>(x.Value!)),
+            UpdateEvent<TIn> x => ChangeEvent.Update(mapper.Map<TOut>(x.OldValue!), mapper.Map<TOut>(x.NewValue!)),
+            DeleteEvent<TIn> x => ChangeEvent.Delete(mapper.Map<TOut>(x.Value!)),
+            _ => throw new NotImplementedException()
+        };
+    }
+}
--- server/Data/src/Annium.Data.Tables/TableExtensions.cs
+++ server/Data/src/Annium.Data.Tables/TableExtensions.cs
@@ -4,45 +4,65 @@ namespace Annium.Data.Tables
 {
     public static class TableExtensions
     {
-        public static IDisposable WriteTo<T>(this IObservable<IChangeEvent<T>> source, ITableSource<T> target) where T : IEquatable<T> =>
-            source.Subscribe(e =>
-            {
-                switch (e)
-                {
-                    case InitEvent<T> init:
-                        target.Init(init.Values);
-                        break;
-                    case AddEvent<T> add:
-                        target.Set(add.Value);
-                        break;
-                    case UpdateEvent<T> update:
-                        target.Set(update.NewValue);
-                        break;
-                    case DeleteEvent<T> delete:
-                        target.Delete(delete.Value);
-                        break;
-                }
-            });
+        public static IDisposable WriteTo<T>(
+            this IObservable<IChangeEvent<T>> source,
+            ITableSource<T> target
+        )
+            where T : IEquatable<T> =>
+            source.Subscribe(target.Write);
 
-        public static IDisposable AppendTo<T>(this IObservable<IChangeEvent<T>> source, ITableSource<T> target) where T : IEquatable<T> =>
-            source.Subscribe(e =>
+        public static IDisposable AppendTo<T>(
+            this IObservable<IChangeEvent<T>> source,
+            ITableSource<T> target
+        )
+            where T : IEquatable<T> =>
+            source.Subscribe(target.Append);
+
+        public static void Write<T>(
+            this ITableSource<T> target,
+            IChangeEvent<T> e
+        )
+            where T : IEquatable<T>
+        {
+            switch (e)
             {
-                switch (e)
-                {
-                    case InitEvent<T> init:
-                        foreach (var value in init.Values)
-                            target.Set(value);
-                        break;
-                    case AddEvent<T> add:
-                        target.Set(add.Value);
-                        break;
-                    case UpdateEvent<T> update:
-                        target.Set(update.NewValue);
-                        break;
-                    case DeleteEvent<T> delete:
-                        target.Delete(delete.Value);
-                        break;
-                }
-            });
+                case InitEvent<T> init:
+                    target.Init(init.Values);
+                    break;
+                case AddEvent<T> add:
+                    target.Set(add.Value);
+                    break;
+                case UpdateEvent<T> update:
+                    target.Set(update.NewValue);
+                    break;
+                case DeleteEvent<T> delete:
+                    target.Delete(delete.Value);
+                    break;
+            }
+        }
+
+        public static void Append<T>(
+            this ITableSource<T> target,
+            IChangeEvent<T> e
+        )
+            where T : IEquatable<T>
+        {
+            switch (e)
+            {
+                case InitEvent<T> init:
+                    foreach (var value in init.Values)
+                        target.Set(value);
+                    break;
+                case AddEvent<T> add:
+                    target.Set(add.Value);
+                    break;
+                case UpdateEvent<T> update:
+                    target.Set(update.NewValue);
+                    break;
+                case DeleteEvent<T> delete:
+                    target.Delete(delete.Value);
+                    break;
+            }
+        }
     }
 }
