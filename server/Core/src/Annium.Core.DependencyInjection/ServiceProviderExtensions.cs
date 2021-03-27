58a5204fcdf2727f3dabf93c4a10169f25489f10
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-03-27 19:51:03 +0300

    Core.DependencyInjection: add ResolveKeyed extension

--- server/Core/src/Annium.Core.DependencyInjection/ServiceProviderExtensions.cs
+++ server/Core/src/Annium.Core.DependencyInjection/ServiceProviderExtensions.cs
@@ -5,8 +5,19 @@ namespace Annium.Core.DependencyInjection
 {
     public static class ServiceProviderExtensions
     {
-        public static T Resolve<T>(this IServiceProvider provider) where T : notnull => provider.GetRequiredService<T>();
-        public static object Resolve(this IServiceProvider provider, Type type) => provider.GetRequiredService(type);
-        public static IServiceScope CreateScope(this IServiceProvider provider) => ServiceProviderServiceExtensions.CreateScope(provider);
+        public static T Resolve<T>(this IServiceProvider provider)
+            where T : notnull
+            => provider.GetRequiredService<T>();
+
+        public static object Resolve(this IServiceProvider provider, Type type)
+            => provider.GetRequiredService(type);
+
+        public static T ResolveKeyed<TKey, T>(this IServiceProvider provider, TKey key)
+            where TKey : notnull
+            where T : notnull
+            => provider.GetRequiredService<IIndex<TKey, T>>()[key];
+
+        public static IServiceScope CreateScope(this IServiceProvider provider)
+            => ServiceProviderServiceExtensions.CreateScope(provider);
     }
 }
