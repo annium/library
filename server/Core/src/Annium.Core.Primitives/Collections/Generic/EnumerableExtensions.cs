e232d05ebc5e49d73bd02769b3f26ef39d9325d9
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-03-28 01:05:41 +0300

    Core.Primitives: add Yield

--- server/Core/src/Annium.Core.Primitives/Collections/Generic/EnumerableExtensions.cs
+++ server/Core/src/Annium.Core.Primitives/Collections/Generic/EnumerableExtensions.cs
@@ -6,5 +6,13 @@ namespace Annium.Core.Primitives.Collections.Generic
     {
         public static string Join(this IEnumerable<string> src, string separator = "") => string.Join(separator, src);
         public static string Join(this IEnumerable<string> src, char separator = ' ') => string.Join(separator, src);
+
+        public static IEnumerable<T> Yield<T>(this T src)
+        {
+            if (src is null)
+                yield break;
+
+            yield return src;
+        }
     }
 }
