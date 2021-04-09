558b6eb14ccd58e706d4d8c4149d23d7d4317dfe
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-04-09 18:37:26 +0300

    Core.DependencyInjection: add IndexExtensions

---
+++ server/Core/src/Annium.Core.DependencyInjection/Extensions/IndexExtensions.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Annium.Core.DependencyInjection.Internal.Builders.Registrations;
+using Annium.Core.DependencyInjection.Internal.Container;
+
+namespace Annium.Core.DependencyInjection
+{
+    public static class IndexExtensions
+    {
+        public static IIndex<TKey, TElement> ToIndex<TSource, TKey, TElement>(
+            this IEnumerable<TSource> src,
+            Func<TSource, TKey> keySelector,
+            Func<TSource, TElement> elementSelector
+        )
+            where TKey : notnull
+            where TElement : notnull
+        {
+            return new Index<TKey, TElement>(src.Select(x => new KeyValue<TKey, TElement>(keySelector(x), elementSelector(x))));
+        }
+    }
+}
