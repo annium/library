db8ad5bcca89a8287c6cb33e5f26f35c963ac1fb
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-04-14 10:55:48 +0300

    Core.Mapper: optimize static Mapper.GetFor

--- server/Core/src/Annium.Core.Mapper/Mapper.cs
+++ server/Core/src/Annium.Core.Mapper/Mapper.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using System.Reflection;
 using Annium.Core.DependencyInjection;
 
@@ -5,15 +6,17 @@ namespace Annium.Core.Mapper
 {
     public static class Mapper
     {
-        public static IMapper GetFor(Assembly assembly)
+        private static readonly ConcurrentDictionary<Assembly, IMapper> Mappers = new();
+
+        public static IMapper GetFor(Assembly assembly) => Mappers.GetOrAdd(assembly, x =>
         {
             var container = new ServiceContainer();
-            container.AddRuntimeTools(assembly, false);
+            container.AddRuntimeTools(x, false);
             container.AddMapper(false);
 
             var provider = container.BuildServiceProvider();
 
             return provider.Resolve<IMapper>();
-        }
+        });
     }
 }
