8e882eed997d9eca0bef42c98ab8cdf73e591b0f
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-04-25 02:48:11 +0300

    Core.Primitives: fix IPEndPointExt

--- server/Core/src/Annium.Core.Primitives/Net/IPEndPointExt.cs
+++ server/Core/src/Annium.Core.Primitives/Net/IPEndPointExt.cs
@@ -1,23 +1,29 @@
 using System;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 
 namespace Annium.Core.Primitives
 {
     // ReSharper disable once InconsistentNaming
     public static class IPEndPointExt
     {
+        private const string TcpPrefix = "tcp://";
+
         public static IPEndPoint Parse(string s, int defaultPort = 0)
         {
             if (!IsValidPort(defaultPort))
                 throw new ArgumentOutOfRangeException(nameof(defaultPort));
 
-            if (!Uri.TryCreate(s, UriKind.Absolute, out var uri))
+            if (!Uri.TryCreate(s.StartsWith(TcpPrefix) ? s : $"{TcpPrefix}{s}", UriKind.Absolute, out var uri))
                 return new IPEndPoint(IPAddress.Loopback, defaultPort);
 
             var port = IsValidPort(uri.Port) ? uri.Port : defaultPort;
             if (uri.Host.Any(char.IsLetter))
-                return new IPEndPoint(Dns.GetHostAddresses(uri.Host).First(), port);
+                return new IPEndPoint(
+                    Dns.GetHostAddresses(uri.Host).First(x => x.AddressFamily == AddressFamily.InterNetwork),
+                    port
+                );
 
             if (IPAddress.TryParse(uri.Host, out var ipAddress))
                 return new IPEndPoint(ipAddress, port);
