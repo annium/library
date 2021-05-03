77c34c3a8050b14ff0468524de28605d2538902d
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-05-03 11:54:22 +0300

    Core.Primitives: add HashCodeSeq implementation for KeyValuePair

--- server/Core/src/Annium.Core.Primitives/HashCodeSeq.cs
+++ server/Core/src/Annium.Core.Primitives/HashCodeSeq.cs
@@ -1,17 +1,32 @@
+using System;
 using System.Collections.Generic;
 
 namespace Annium.Core.Primitives
 {
     public static class HashCodeSeq
     {
-        public static int Combine<T>(IEnumerable<T> seq)
-            where T : notnull
+        public static int Combine<TKey, TValue>(IEnumerable<KeyValuePair<TKey, TValue>> seq)
+            where TKey : notnull
         {
             unchecked
             {
                 var hash = 19;
 
                 foreach (var x in seq)
+                    hash = hash * 31 + HashCode.Combine(x.Key.GetHashCode(), x.Value.GetHashCode());
+
+                return hash;
+            }
+        }
+
+        public static int Combine<T>(IEnumerable<T> seq)
+            where T : notnull
+        {
+            unchecked
+            {
+                var hash = 19;
+                foreach (var x in seq)
+
                     hash = hash * 31 + x.GetHashCode();
 
                 return hash;
