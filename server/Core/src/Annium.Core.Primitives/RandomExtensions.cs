a3c728f077a74939949a4a83c0e1e2bda84bec67
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-05-10 15:19:52 +0300

    Core.Primitives: add randomization extensions

--- server/Core/src/Annium.Core.Primitives/RandomExtensions.cs
+++ server/Core/src/Annium.Core.Primitives/RandomExtensions.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Linq;
 
 namespace Annium.Core.Primitives
 {
     public static class RandomExtensions
     {
+        public static bool NextBool(this Random random) => random.Next(0, 1) == 1;
+
+        public static T NextEnum<T>(this Random random) where T : Enum
+        {
+            var values = Enum.GetValues(typeof(T)).OfType<T>().ToArray();
+
+            return values[random.Next(0, values.Length)];
+        }
+
         public static decimal NextDecimal(this Random random) => (decimal) random.NextDouble();
     }
 }
