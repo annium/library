45c0b5b977eb7e5e288536c7b85735c439d40497
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-05-10 16:29:46 +0300

    Core.Primitives: update Random.NextEnum extension

--- server/Core/src/Annium.Core.Primitives/RandomExtensions.cs
+++ server/Core/src/Annium.Core.Primitives/RandomExtensions.cs
@@ -7,9 +7,10 @@ namespace Annium.Core.Primitives
     {
         public static bool NextBool(this Random random) => random.Next(0, 1) == 1;
 
-        public static T NextEnum<T>(this Random random) where T : Enum
+        public static T NextEnum<T>(this Random random, params T[] values) where T : Enum
         {
-            var values = Enum.GetValues(typeof(T)).OfType<T>().ToArray();
+            if (values.Length == 0)
+                values = Enum.GetValues(typeof(T)).OfType<T>().ToArray();
 
             return values[random.Next(0, values.Length)];
         }
