4e2a3d36e479a15cb54d08659525e1f3638a4d9a
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-05-13 00:49:20 +0300

    Core.Runtime: cache TypeId.Create

--- server/Core/src/Annium.Core.Runtime/Types/TypeExtensions.cs
+++ server/Core/src/Annium.Core.Runtime/Types/TypeExtensions.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections.Concurrent;
 
 namespace Annium.Core.Runtime.Types
 {
     public static class TypeExtensions
     {
-        public static TypeId GetId(this Type type) => TypeId.Create(type);
+        private static readonly ConcurrentDictionary<Type, TypeId> TypeIds = new();
+        public static TypeId GetId(this Type type) => TypeIds.GetOrAdd(type, TypeId.Create);
         public static string GetIdString(this Type type) => TypeId.Create(type).Id;
     }
 }
