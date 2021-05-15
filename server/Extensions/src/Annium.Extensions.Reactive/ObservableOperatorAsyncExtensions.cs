6f5d7a335c96f61e0e49e35a94c2ac60fde8d45a
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-05-15 19:58:39 +0300

    Extensions.Reactive: split SelectAsync to SelectParallelAsync and SelectSequentialAsync

--- server/Extensions/src/Annium.Extensions.Reactive/ObservableOperatorAsyncExtensions.cs
+++ server/Extensions/src/Annium.Extensions.Reactive/ObservableOperatorAsyncExtensions.cs
@@ -1,3 +1,4 @@
+using System.Reactive;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 
@@ -5,17 +6,53 @@ namespace System
 {
     public static class ObservableOperatorAsyncExtensions
     {
-        public static IObservable<TResult> SelectAsync<TSource, TResult>(
+        public static IObservable<TResult> SelectParallelAsync<TSource, TResult>(
             this IObservable<TSource> source,
             Func<TSource, Task<TResult>> selector
         )
             => source.SelectMany(x => Observable.FromAsync(() => selector(x)));
 
-        public static IObservable<TResult> SelectAsync<TSource, TResult>(
+        public static IObservable<TResult> SelectParallelAsync<TSource, TResult>(
             this IObservable<TSource> source,
             Func<TSource, int, Task<TResult>> selector
         )
             => source.SelectMany((x, i) => Observable.FromAsync(() => selector(x, i)));
+
+        public static IObservable<Unit> SelectParallelAsync<TSource>(
+            this IObservable<TSource> source,
+            Func<TSource, Task> selector
+        )
+            => source.SelectMany(x => Observable.FromAsync(() => selector(x)));
+
+        public static IObservable<Unit> SelectParallelAsync<TSource>(
+            this IObservable<TSource> source,
+            Func<TSource, int, Task> selector
+        )
+            => source.SelectMany((x, i) => Observable.FromAsync(() => selector(x, i)));
+
+        public static IObservable<TResult> SelectSequentialAsync<TSource, TResult>(
+            this IObservable<TSource> source,
+            Func<TSource, Task<TResult>> selector
+        )
+            => source.Select(x => Observable.FromAsync(() => selector(x))).Concat();
+
+        public static IObservable<TResult> SelectSequentialAsync<TSource, TResult>(
+            this IObservable<TSource> source,
+            Func<TSource, int, Task<TResult>> selector
+        )
+            => source.Select((x, i) => Observable.FromAsync(() => selector(x, i))).Concat();
+
+        public static IObservable<Unit> SelectSequentialAsync<TSource>(
+            this IObservable<TSource> source,
+            Func<TSource, Task> selector
+        )
+            => source.Select(x => Observable.FromAsync(() => selector(x))).Concat();
+
+        public static IObservable<Unit> SelectSequentialAsync<TSource>(
+            this IObservable<TSource> source,
+            Func<TSource, int, Task> selector
+        )
+            => source.Select((x, i) => Observable.FromAsync(() => selector(x, i))).Concat();
 
         public static IObservable<TSource> CatchAsync<TSource, TException>(
             this IObservable<TSource> source,
--- server/Extensions/test/Annium.Extensions.Reactive.Tests/ObservableAsyncOperatorExtensionsTest.cs
+++ server/Extensions/test/Annium.Extensions.Reactive.Tests/ObservableAsyncOperatorExtensionsTest.cs
@@ -11,13 +11,13 @@ namespace Annium.Extensions.Reactive.Tests
     public class ObservableAsyncOperatorExtensionsTest
     {
         [Fact]
-        public async Task SelectAsync_WorksCorrectly()
+        public async Task SelectParallelAsync_WorksCorrectly()
         {
             // arrange
             var log = new List<string>();
             var tcs = new TaskCompletionSource();
             using var observable = Observable.Range(1, 5)
-                .SelectAsync(async x =>
+                .SelectParallelAsync(async x =>
                 {
                     lock (log)
                         log.Add($"start: {x}");
@@ -39,6 +39,43 @@ namespace Annium.Extensions.Reactive.Tests
             var ends = log.Select((x, i) => (x, i)).Where(x => !x.x.StartsWith("start")).Select(x => x.i).ToArray();
             // ends/subs go after all starts
             ends.All(x => starts.All(s => s < x)).IsTrue();
+        }
+
+        [Fact]
+        public async Task SelectSequentialAsync_WorksCorrectly()
+        {
+            // arrange
+            var log = new List<string>();
+            var tcs = new TaskCompletionSource();
+            using var observable = Observable.Range(1, 5)
+                .SelectSequentialAsync(async x =>
+                {
+                    lock (log)
+                        log.Add($"start: {x}");
+                    await Task.Delay(10);
+                    lock (log)
+                        log.Add($"end: {x}");
+                    return x;
+                })
+                .Subscribe(x =>
+                {
+                    lock (log)
+                        log.Add($"sub: {x}");
+                }, tcs.SetResult);
+
+            await tcs.Task;
+
+            log.Has(15);
+            var expectedLog = Enumerable.Range(1, 5)
+                .Select(x => new[]
+                {
+                    $"start: {x}",
+                    $"end: {x}",
+                    $"sub: {x}"
+                })
+                .SelectMany(x => x)
+                .ToArray();
+            log.IsEqual(expectedLog);
         }
 
         [Fact]
