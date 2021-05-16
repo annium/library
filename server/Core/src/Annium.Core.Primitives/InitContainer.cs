a580adbcf972bf5fa4f8cce1cc7a537a6f01a034
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-05-16 23:50:50 +0300

    Core.Primitives: implement InitContainer

---
+++ server/Core/src/Annium.Core.Primitives/InitContainer.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Annium.Core.Primitives
+{
+    public class InitContainer<T>
+    {
+        public T Value
+        {
+            get
+            {
+                EnsureInitiated();
+                return _get();
+            }
+        }
+
+        private AsyncLazy<T>? _initiator;
+
+        // private
+        private readonly Func<T> _get;
+        private readonly Action<T> _set;
+
+        public InitContainer()
+        {
+            var value = default(T)!;
+            _get = () => value;
+            _set = x => value = x;
+        }
+
+        public InitContainer(Func<T> get, Action<T> set)
+        {
+            _get = get;
+            _set = set;
+        }
+
+        public void SetInit(Func<Task<T>> init)
+        {
+            if (_initiator is not null)
+                throw new InvalidOperationException("Container is already initiated");
+
+            _initiator = new AsyncLazy<T>(async () =>
+            {
+                var value = await init();
+                _set(value);
+
+                return value;
+            });
+        }
+
+        public void Set(T value)
+        {
+            EnsureInitiated();
+
+            _set(value);
+        }
+
+        public TaskAwaiter<T> GetAwaiter()
+        {
+            EnsureInitiatorSet();
+
+            return _initiator!.GetAwaiter();
+        }
+
+        private void EnsureInitiated()
+        {
+            EnsureInitiatorSet();
+
+            if (!_initiator!.IsValueCreated)
+                throw new InvalidOperationException("Container is not initiated");
+        }
+
+        private void EnsureInitiatorSet()
+        {
+            if (_initiator is null)
+                throw new InvalidOperationException("Container initiator is not set");
+        }
+
+        public static implicit operator T(InitContainer<T> container) => container.Value;
+    }
+}
---
+++ server/Core/test/Annium.Core.Primitives.Tests/InitContainerTest.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Threading.Tasks;
+using Annium.Testing;
+using Xunit;
+
+namespace Annium.Core.Primitives.Tests
+{
+    public class InitContainerTest
+    {
+        [Fact]
+        public void Managed_NotInitiated_Get_Throws()
+        {
+            // arrange
+            var container = new InitContainer<int>();
+
+            // assert
+            ((Action) (() =>
+            {
+                var _ = container.Value;
+            })).Throws<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void Managed_NotInitiated_Set_Throws()
+        {
+            // arrange
+            var container = new InitContainer<int>();
+
+            // assert
+            ((Action) (() => { container.Set(5); })).Throws<InvalidOperationException>();
+        }
+
+        [Fact]
+        public async Task Managed_Init_ReturnsGetterResult()
+        {
+            // arrange
+            var val = 2;
+            var container = new InitContainer<int>(() => val, x => val = x);
+            container.SetInit(async () =>
+            {
+                await Task.Delay(100);
+                return 5;
+            });
+
+            // act
+            var initial = await container;
+
+            // assert
+            initial.Is(5);
+            val.Is(5);
+            container.Value.Is(5);
+        }
+
+        [Fact]
+        public async Task Managed_Set_Works()
+        {
+            // arrange
+            var val = 2;
+            var container = new InitContainer<int>(() => val, x => val = x);
+            container.SetInit(async () =>
+            {
+                await Task.Delay(100);
+                return 5;
+            });
+
+            // act
+            await container;
+            container.Set(3);
+
+            // assert
+            container.Value.Is(3);
+            ((int) container).Is(3);
+        }
+
+        [Fact]
+        public async Task Unmanaged_Init_ReturnsGetterResult()
+        {
+            // arrange
+            var container = new InitContainer<int>();
+            container.SetInit(async () =>
+            {
+                await Task.Delay(100);
+                return 5;
+            });
+
+            // act
+            var initial = await container;
+
+            // assert
+            initial.Is(5);
+            container.Value.Is(5);
+        }
+
+        [Fact]
+        public async Task Unmanaged_Set_Works()
+        {
+            // arrange
+            var container = new InitContainer<int>();
+            container.SetInit(async () =>
+            {
+                await Task.Delay(100);
+                return 5;
+            });
+
+            // act
+            await container;
+            container.Set(3);
+
+            // assert
+            container.Value.Is(3);
+            ((int) container).Is(3);
+        }
+    }
+}
