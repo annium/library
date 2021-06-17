43df361ba260101b8a329bb4981732f8b61a150c
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-06-17 23:19:52 +0300

    Diagnostics.Debug: update test

--- server/Diagnostics/test/Annium.Diagnostics.Debug.Tests/IdExtensionsTest.cs
+++ server/Diagnostics/test/Annium.Diagnostics.Debug.Tests/IdExtensionsTest.cs
@@ -6,16 +6,19 @@ namespace Annium.Diagnostics.Debug.Tests
     public class IdExtensionsTest
     {
         [Fact]
-        public void GetId_IsStablyUniquePerObject()
+        public void GetId_IsStablyUniquePerObject_NotSharedAmongTypes()
         {
             // arrange
             var a = new object();
             var b = new object();
+            var c = new { };
 
             // assert
             string.IsNullOrWhiteSpace(a.GetId()).IsFalse();
             (a.GetId() == a.GetId()).IsTrue();
-            (a.GetId() != b.GetId()).IsTrue();
+            a.GetId().Is("001");
+            b.GetId().Is("002");
+            c.GetId().Is("001");
         }
 
         private class Demo
