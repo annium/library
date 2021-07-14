da5238b77ae25019d3ad18d38419387521a6b1f1
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-07-14 18:31:43 +0300

    Core.Primitives: add Chunks Enumerable extension

--- server/Core/src/Annium.Core.Primitives/Collections/Generic/EnumerableExtensions.cs
+++ server/Core/src/Annium.Core.Primitives/Collections/Generic/EnumerableExtensions.cs
@@ -10,6 +10,25 @@ namespace Annium.Core.Primitives
         public static string Join(this IEnumerable<string> src, string separator = "") => string.Join(separator, src);
         public static string Join(this IEnumerable<string> src, char separator = ' ') => string.Join(separator, src);
 
+        public static IEnumerable<IReadOnlyCollection<T>> Chunks<T>(this IEnumerable<T> src, int chunkSize)
+        {
+            var chunk = new List<T>();
+
+            foreach (var element in src)
+            {
+                if (chunk.Count == chunkSize)
+                {
+                    yield return chunk;
+                    chunk = new List<T>();
+                }
+
+                chunk.Add(element);
+            }
+
+            if (chunk.Count > 0)
+                yield return chunk;
+        }
+
         public static IEnumerable<T> Yield<T>(this T src)
         {
             if (src is null)
