6d98e42dc7888d33ad5d3eee3d2c66cefa2da2b3
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-09-04 11:23:44 +0300

    Core.Runtime: extend ManagedTimeProviderExtensions

--- server/Core/src/Annium.Core.Runtime/Time/ManagedTimeProviderExtensions.cs
+++ server/Core/src/Annium.Core.Runtime/Time/ManagedTimeProviderExtensions.cs
@@ -6,5 +6,20 @@ namespace Annium.Core.Runtime.Time
     {
         public static void AddMinute(this IManagedTimeProvider timeProvider) =>
             timeProvider.SetNow(timeProvider.Now + Duration.FromMinutes(1));
+
+        public static void AddMinutes(this IManagedTimeProvider timeProvider, int minutes) =>
+            timeProvider.SetNow(timeProvider.Now + Duration.FromMinutes(minutes));
+
+        public static void AddHour(this IManagedTimeProvider timeProvider) =>
+            timeProvider.SetNow(timeProvider.Now + Duration.FromHours(1));
+
+        public static void AddHours(this IManagedTimeProvider timeProvider, int hours) =>
+            timeProvider.SetNow(timeProvider.Now + Duration.FromHours(hours));
+
+        public static void AddDay(this IManagedTimeProvider timeProvider) =>
+            timeProvider.SetNow(timeProvider.Now + Duration.FromDays(1));
+
+        public static void AddDays(this IManagedTimeProvider timeProvider, int days) =>
+            timeProvider.SetNow(timeProvider.Now + Duration.FromDays(days));
     }
 }
