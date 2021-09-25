aa95f3ef437e8cb5022bf9228c51fe26eca04034
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-09-25 12:58:46 +0300

    Core.Primitives: fix IdExtensions

--- server/Core/src/Annium.Core.Primitives/IdExtensions.cs
+++ server/Core/src/Annium.Core.Primitives/IdExtensions.cs
@@ -5,6 +5,6 @@ namespace Annium.Core.Primitives
     public static class IdExtensions
     {
         public static string GetFullId<T>(this T obj) where T : class =>
-            obj is null! ? "null" : $"{typeof(T).FriendlyName()}#{obj.GetId()}";
+            obj is null! ? "null" : $"{obj.GetType().FriendlyName()}#{obj.GetId()}";
     }
 }
