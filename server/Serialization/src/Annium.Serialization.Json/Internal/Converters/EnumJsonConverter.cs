5e1656a9ac8ec89ab645121a49b93a853d16e5e4
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-09-26 20:45:37 +0300

    Serialization.Json: fix EnumJsonConverter

--- server/Serialization/src/Annium.Serialization.Json/Internal/Converters/EnumJsonConverter.cs
+++ server/Serialization/src/Annium.Serialization.Json/Internal/Converters/EnumJsonConverter.cs
@@ -32,7 +32,7 @@ namespace Annium.Serialization.Json.Internal.Converters
 
         public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
         {
-            writer.WriteNumberValue((long) Convert.ChangeType(value, typeof(long)));
+            writer.WriteNumberValue((long)Convert.ChangeType(value, typeof(long)));
         }
     }
 
@@ -63,7 +63,7 @@ namespace Annium.Serialization.Json.Internal.Converters
 
         public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
         {
-            writer.WriteNumberValue(long.Parse(value.ToString()));
+            writer.WriteNumberValue((long)Convert.ChangeType(value, typeof(long)));
         }
     }
 }
