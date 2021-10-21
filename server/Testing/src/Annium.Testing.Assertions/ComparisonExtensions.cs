c7cda6808281a5965d65c009625a4dedd332cca3
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-10-21 13:03:53 +0300

    Testing.Assertions: add ComparisonExtensions

---
+++ server/Testing/src/Annium.Testing.Assertions/ComparisonExtensions.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Text.Json;
+
+namespace Annium.Testing
+{
+    public static class ComparisonExtensions
+    {
+        public static void IsLess<T>(this T value, T data, string message = "")
+            where T : IComparable<T>
+        {
+            if (value.CompareTo(data) >= 0)
+                throw new AssertionFailedException(
+                    string.IsNullOrEmpty(message)
+                        ? $"{JsonSerializer.Serialize(value)} is not less than {JsonSerializer.Serialize(data)}"
+                        : message
+                );
+        }
+
+        public static void IsLessOrEqual<T>(this T value, T data, string message = "")
+            where T : IComparable<T>
+        {
+            if (value.CompareTo(data) > 0)
+                throw new AssertionFailedException(
+                    string.IsNullOrEmpty(message)
+                        ? $"{JsonSerializer.Serialize(value)} is not less or equal to {JsonSerializer.Serialize(data)}"
+                        : message
+                );
+        }
+
+        public static void IsGreater<T>(this T value, T data, string message = "")
+            where T : IComparable<T>
+        {
+            if (value.CompareTo(data) <= 0)
+                throw new AssertionFailedException(
+                    string.IsNullOrEmpty(message)
+                        ? $"{JsonSerializer.Serialize(value)} is not greater than {JsonSerializer.Serialize(data)}"
+                        : message
+                );
+        }
+
+        public static void IsGreaterOrEqual<T>(this T value, T data, string message = "")
+            where T : IComparable<T>
+        {
+            if (value.CompareTo(data) < 0)
+                throw new AssertionFailedException(
+                    string.IsNullOrEmpty(message)
+                        ? $"{JsonSerializer.Serialize(value)} is not greater or equal to {JsonSerializer.Serialize(data)}"
+                        : message
+                );
+        }
+    }
+}
