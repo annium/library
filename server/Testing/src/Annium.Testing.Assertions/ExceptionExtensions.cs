4dd6525f91ccd1d332867c96f63074e6599ea8c3
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-10-26 13:00:59 +0300

    Testing.Assertions: improve Throws/Reports

--- server/Testing/src/Annium.Testing.Assertions/ExceptionExtensions.cs
+++ server/Testing/src/Annium.Testing.Assertions/ExceptionExtensions.cs
@@ -1,17 +1,29 @@
 using System;
-using System.Reflection;
-using Annium.Core.Mapper;
+using System.Linq;
+using Annium.Core.Primitives.Collections.Generic;
 
 namespace Annium.Testing
 {
     public static class ExceptionExtensions
     {
-        public static T Reports<T>(this T value, string message) where T : Exception
+        public static T Reports<T>(this T value, string message, params string[] messages)
+            where T : Exception
         {
-            value.Message.IsEqual(
+            var allMessages = message.Yield().Concat(messages).ToArray();
+            if (!allMessages.All(value.Message.Contains))
+                throw new AssertionFailedException(
+                    $"Expected exception message `{value.Message}` to contain: `{allMessages.Join("`, `")}`"
+                );
+
+            return value;
+        }
+
+        public static T ReportsExactly<T>(this T value, string message)
+            where T : Exception
+        {
+            value.Message.Is(
                 message,
-                Mapper.GetFor(Assembly.GetCallingAssembly()),
-                $"Expected exception message `{message}`, got `{value.Message}`"
+                $"Expected exception message `{value.Message}` to be `{message}`"
             );
 
             return value;
