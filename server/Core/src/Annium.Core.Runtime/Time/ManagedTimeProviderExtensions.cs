908a63db4818380b747d540bdadbd132843c173d
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-10-28 23:35:58 +0300

    Core.Runtime: extend ManagerdTimeProviderExtensions

--- server/Core/src/Annium.Core.Runtime/Time/ManagedTimeProviderExtensions.cs
+++ server/Core/src/Annium.Core.Runtime/Time/ManagedTimeProviderExtensions.cs
@@ -4,22 +4,28 @@ namespace Annium.Core.Runtime.Time
 {
     public static class ManagedTimeProviderExtensions
     {
-        public static void AddMinute(this IManagedTimeProvider timeProvider) =>
-            timeProvider.SetNow(timeProvider.Now + Duration.FromMinutes(1));
+        public static void AddSecond(this IManagedTimeProvider timeProvider) =>
+            timeProvider.SetNow(timeProvider.Now + Duration.FromSeconds(1L));
 
-        public static void AddMinutes(this IManagedTimeProvider timeProvider, int minutes) =>
+        public static void AddSeconds(this IManagedTimeProvider timeProvider, long seconds) =>
+            timeProvider.SetNow(timeProvider.Now + Duration.FromSeconds(seconds));
+
+        public static void AddMinute(this IManagedTimeProvider timeProvider) =>
+            timeProvider.SetNow(timeProvider.Now + Duration.FromMinutes(1L));
+
+        public static void AddMinutes(this IManagedTimeProvider timeProvider, long minutes) =>
             timeProvider.SetNow(timeProvider.Now + Duration.FromMinutes(minutes));
 
         public static void AddHour(this IManagedTimeProvider timeProvider) =>
-            timeProvider.SetNow(timeProvider.Now + Duration.FromHours(1));
+            timeProvider.SetNow(timeProvider.Now + Duration.FromHours(1L));
 
-        public static void AddHours(this IManagedTimeProvider timeProvider, int hours) =>
+        public static void AddHours(this IManagedTimeProvider timeProvider, long hours) =>
             timeProvider.SetNow(timeProvider.Now + Duration.FromHours(hours));
 
         public static void AddDay(this IManagedTimeProvider timeProvider) =>
-            timeProvider.SetNow(timeProvider.Now + Duration.FromDays(1));
+            timeProvider.SetNow(timeProvider.Now + Duration.FromDays(1L));
 
-        public static void AddDays(this IManagedTimeProvider timeProvider, int days) =>
+        public static void AddDays(this IManagedTimeProvider timeProvider, long days) =>
             timeProvider.SetNow(timeProvider.Now + Duration.FromDays(days));
     }
 }
