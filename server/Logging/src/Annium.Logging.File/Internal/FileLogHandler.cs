7b7ffe9a9fc6299e2d161c3d7eaa699d32b2ec8e
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-11-08 19:48:36 +0300

    Logging.File: fix FileLogHandler - handle concurrency and avoid line merges

--- server/Logging/src/Annium.Logging.File/Internal/FileLogHandler.cs
+++ server/Logging/src/Annium.Logging.File/Internal/FileLogHandler.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Annium.Logging.Shared;
 
@@ -8,6 +9,7 @@ namespace Annium.Logging.File.Internal
     internal class FileLogHandler<TContext> : BufferingLogHandler<TContext, string>
         where TContext : class, ILogContext
     {
+        private readonly AutoResetEvent _gate = new(true);
         private readonly FileLoggingConfiguration _cfg;
 
         public FileLogHandler(
@@ -23,13 +25,19 @@ namespace Annium.Logging.File.Internal
         {
             try
             {
+                _gate.WaitOne();
                 await System.IO.File.AppendAllLinesAsync(_cfg.File, events);
+                await System.IO.File.AppendAllTextAsync(_cfg.File, string.Empty);
                 return true;
             }
             catch (Exception e)
             {
                 Console.WriteLine($"Failed to to write events to {_cfg.File}: {e}");
                 return false;
+            }
+            finally
+            {
+                _gate.Set();
             }
         }
     }
