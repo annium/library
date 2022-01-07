7c573a978fb944fdbb57a79ce7cd904a1d00a38f
Alex Kreskiyan <a.kreskiyan@gmail.com>
2022-01-07 13:46:28 +0300

    Logging.Shared: add Logging.Abstractions as Asm dependency

--- server/Logging/src/Annium.Logging.Shared/Asm.cs
+++ server/Logging/src/Annium.Logging.Shared/Asm.cs
@@ -1,5 +1,6 @@
 using System.Runtime.CompilerServices;
 using Annium.Core.Runtime.Types;
+using Annium.Logging.Abstractions;
 
-[assembly: AutoScanned]
+[assembly: AutoScanned(typeof(LogLevel))]
 [assembly: InternalsVisibleTo("Annium.Logging.Shared.Tests")]
