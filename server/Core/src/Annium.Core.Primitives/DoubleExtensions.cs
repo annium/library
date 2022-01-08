a1f75d8d5af76fcffe70491e03309b3dbca25309
Alex Kreskiyan <a.kreskiyan@gmail.com>
2022-01-08 18:41:24 +0300

    Core.Primitives: add float/double extensions

--- server/Core/src/Annium.Core.Primitives/DoubleExtensions.cs
+++ server/Core/src/Annium.Core.Primitives/DoubleExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.CompilerServices;
 
 namespace Annium.Core.Primitives;
 
@@ -7,6 +8,15 @@ public static class DoubleExtensions
     public static double DiffFrom(this double value, double from) =>
         from == 0d ? double.PositiveInfinity : Math.Abs((value - from) / from);
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool IsAround(this double value, double to, double precision) =>
         value.DiffFrom(to) <= precision;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int FloorInt32(this double value) =>
+        (int)Math.Floor(value);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static long FloorInt64(this double value) =>
+        (int)Math.Floor(value);
 }
--- server/Core/src/Annium.Core.Primitives/FloatExtensions.cs
+++ server/Core/src/Annium.Core.Primitives/FloatExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.CompilerServices;
 
 namespace Annium.Core.Primitives;
 
@@ -7,6 +8,7 @@ public static class FloatExtensions
     public static float DiffFrom(this float value, float from) =>
         from == 0f ? float.PositiveInfinity : Math.Abs((value - from) / from);
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool IsAround(this float value, float to, float precision) =>
         value.DiffFrom(to) <= precision;
 }
