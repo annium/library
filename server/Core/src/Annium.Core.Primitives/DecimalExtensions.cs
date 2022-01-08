5bced6f3c60fbc13929185b822d7efe149b8989c
Alex Kreskiyan <a.kreskiyan@gmail.com>
2022-01-09 02:39:36 +0300

    Core.Primitives: add decimal extensions for flooring

--- server/Core/src/Annium.Core.Primitives/DecimalExtensions.cs
+++ server/Core/src/Annium.Core.Primitives/DecimalExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.CompilerServices;
 
 namespace Annium.Core.Primitives;
 
@@ -7,6 +8,15 @@ public static class DecimalExtensions
     public static decimal DiffFrom(this decimal value, decimal from) =>
         from == 0m ? decimal.MaxValue : Math.Abs((value - from) / from);
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool IsAround(this decimal value, decimal to, decimal precision) =>
         value.DiffFrom(to) <= precision;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int FloorInt32(this decimal value) =>
+        (int)Math.Floor(value);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static long FloorInt64(this decimal value) =>
+        (long)Math.Floor(value);
 }
