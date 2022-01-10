af8f8741d94499f9a653475ef312f425ea08ecf9
Alex Kreskiyan <a.kreskiyan@gmail.com>
2022-01-10 19:19:56 +0300

    NodaTime.Extensions: fix Duration.AlignTo

--- third-party/NodaTime/src/Annium.NodaTime.Extensions/DurationExtensions.cs
+++ third-party/NodaTime/src/Annium.NodaTime.Extensions/DurationExtensions.cs
@@ -16,6 +16,11 @@ public static class DurationExtensions
     public static Duration AlignToDay(this Duration m) =>
         m.AlignTo(Duration.FromDays(1));
 
-    public static Duration AlignTo(this Duration m, Duration d) =>
-        m - Duration.FromTicks((long)m.TotalTicks % (long)d.TotalTicks);
+    public static Duration AlignTo(this Duration m, Duration d)
+    {
+        var mt = (long)m.TotalTicks;
+        var dt = (long)d.TotalTicks;
+
+        return Duration.FromTicks(mt - mt % dt);
+    }
 }
