337eee7546fde096757c05099b1e18d23660285b
Alex Kreskiyan <a.kreskiyan@gmail.com>
2022-01-15 16:53:34 +0300

    Core.Primitives: add decimal.ToPretty extension

--- server/Core/src/Annium.Core.Primitives/DecimalExtensions.cs
+++ server/Core/src/Annium.Core.Primitives/DecimalExtensions.cs
@@ -6,7 +6,7 @@ namespace Annium.Core.Primitives;
 public static class DecimalExtensions
 {
     public static decimal DiffFrom(this decimal value, decimal from) =>
-        from == 0m ? decimal.MaxValue : Math.Abs((value - from) / from);
+        from == 0m ? decimal.MaxValue : value.DiffFromInternal(from);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool IsAround(this decimal value, decimal to, decimal precision) =>
@@ -59,6 +59,7 @@ public static class DecimalExtensions
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static long RoundInt64(this decimal value, int digits, MidpointRounding mode) =>
         (long)Math.Round(value, digits, mode);
+
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static decimal Within(this decimal value, decimal min, decimal max) => value.Above(min).Below(max);
 
@@ -67,4 +68,42 @@ public static class DecimalExtensions
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static decimal Below(this decimal value, decimal max) => Math.Min(value, max);
+
+    public static decimal ToPretty(this decimal value, decimal diff)
+    {
+        if (value == 0m)
+            return 0;
+
+        var result = value;
+
+        var decimals = value.Decimals();
+        if (decimals > 0)
+        {
+            while (decimals > 0)
+            {
+                var next = Math.Round(result, --decimals);
+                if (next.DiffFromInternal(value) > diff)
+                    return result;
+
+                result = next;
+            }
+        }
+
+        var mul = 10;
+        while (true)
+        {
+            var next = Math.Round(result / mul) * mul;
+            if (next.DiffFromInternal(value) > diff)
+                return result;
+
+            result = next;
+            mul *= 10;
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static decimal DiffFromInternal(this decimal value, decimal from) => Math.Abs((value - from) / from);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static int Decimals(this decimal value) => decimal.GetBits(value)[3] >> 16;
 }
--- server/Core/test/Annium.Core.Primitives.Tests/DecimalExtensionsTest.cs
+++ server/Core/test/Annium.Core.Primitives.Tests/DecimalExtensionsTest.cs
@@ -22,4 +22,13 @@ public class DecimalExtensionsTest
         a.DiffFrom(d).Is(0.01m);
         a.DiffFrom(e).Is(1.99m);
     }
+
+    [Fact]
+    public void ToPretty()
+    {
+        // assert
+        123.9214999m.ToPretty(0.01m).Is(124m);
+        123.9214999m.ToPretty(0.1m).Is(120m);
+        123.9214999m.ToPretty(0.5m).Is(100m);
+    }
 }
