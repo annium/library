081c9fcadeb1c10075d60b92e7845c739a909bda
Alex Kreskiyan <a.kreskiyan@gmail.com>
2022-07-18 08:04:12 +0300

    linq2db.Extensions: split public/internal MappingBuilderExtensions

---
+++ third-party/linq2db/src/Annium.linq2db.Extensions/Internal/MappingBuilderExtensions.cs
@@ -0,0 +1,29 @@
+using System.Linq;
+using Annium.Core.Primitives;
+using LinqToDB.Mapping;
+
+namespace Annium.linq2db.Extensions.Internal;
+
+internal static class MappingBuilderExtensions
+{
+    public static IMappingBuilder IncludeAssociationKeysAsColumns(this IMappingBuilder builder) => builder.Configure(db =>
+    {
+        var fluentBuilder = builder.Map;
+
+        foreach (var table in db.Tables)
+        foreach (var column in table.Columns)
+        {
+            if (column.Attribute.IsColumn)
+                continue;
+
+            // check if is foreign key
+            var isForeignKey = table.Columns
+                .Where(x => x.Attribute.IsColumn)
+                .Any(x => x.Association?.ThisKey == column.Member.Name);
+
+            // set as basic column
+            if (isForeignKey)
+                fluentBuilder.HasAttribute(column.Member, new ColumnAttribute { IsColumn = true });
+        }
+    }, MetadataBuilderFlags.IncludeMembersNotMarkedAsColumns);
+}
--- third-party/linq2db/src/Annium.linq2db.Extensions/MappingBuilderExtensions.cs
+++ third-party/linq2db/src/Annium.linq2db.Extensions/MappingBuilderExtensions.cs
@@ -1,32 +1,9 @@
-using System.Linq;
 using Annium.Core.Primitives;
-using LinqToDB.Mapping;
 
 namespace Annium.linq2db.Extensions;
 
 public static class MappingBuilderExtensions
 {
-    internal static IMappingBuilder IncludeAssociationKeysAsColumns(this IMappingBuilder builder) => builder.Configure(db =>
-    {
-        var fluentBuilder = builder.Map;
-
-        foreach (var table in db.Tables)
-        foreach (var column in table.Columns)
-        {
-            if (column.Attribute.IsColumn)
-                continue;
-
-            // check if is foreign key
-            var isForeignKey = table.Columns
-                .Where(x => x.Attribute.IsColumn)
-                .Any(x => x.Association?.ThisKey == column.Member.Name);
-
-            // set as basic column
-            if (isForeignKey)
-                fluentBuilder.HasAttribute(column.Member, new ColumnAttribute { IsColumn = true });
-        }
-    }, MetadataBuilderFlags.IncludeMembersNotMarkedAsColumns);
-
     public static IMappingBuilder SnakeCaseColumns(this IMappingBuilder builder) => builder.Configure(db =>
     {
         foreach (var table in db.Tables)
