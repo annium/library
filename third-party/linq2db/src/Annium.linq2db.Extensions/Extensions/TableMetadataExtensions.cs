3d2819500e8ec96d8bc404d60f950b92f3605af2
Alex Kreskiyan <a.kreskiyan@gmail.com>
2022-08-02 12:27:00 +0300

    linq2db.Extensions: implement ITable<T> GetMetadata extension

---
+++ third-party/linq2db/src/Annium.linq2db.Extensions/Extensions/TableMetadataExtensions.cs
@@ -0,0 +1,16 @@
+using System;
+using Annium.Core.Primitives;
+using Annium.linq2db.Extensions.Configuration.Extensions;
+using Annium.linq2db.Extensions.Configuration.Metadata;
+using LinqToDB;
+
+namespace Annium.linq2db.Extensions.Extensions;
+
+public static class TableMetadataExtensions
+{
+    public static TableMetadata GetMetadata<T>(this ITable<T> table)
+        where T : notnull
+        => table.DataContext.MappingSchema.CachedDescribe().Tables.TryGetValue(typeof(T), out var tableMetadata)
+            ? tableMetadata
+            : throw new InvalidOperationException($"Type {typeof(T).FriendlyName()} is missing in database metadata");
+}
