0f9d3adc105fbe63526e96dbea1398a2456fa57e
Alex Kreskiyan <a.kreskiyan@gmail.com>
2022-08-09 01:06:43 +0300

    Core.Reflection: fix HasDefaultConstructor to detect non-public constructors

--- server/Core/src/Annium.Core.Reflection/Types/Extensions/HasDefaultConstructorExtension.cs
+++ server/Core/src/Annium.Core.Reflection/Types/Extensions/HasDefaultConstructorExtension.cs
@@ -1,19 +1,22 @@
 using System;
+using System.Reflection;
 
 namespace Annium.Core.Reflection;
 
 public static class HasDefaultConstructorExtension
 {
+    private static readonly BindingFlags BindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+
     public static bool HasDefaultConstructor(this Type type)
     {
         if (type is null)
             throw new ArgumentNullException(nameof(type));
 
         if (type.IsClass)
-            return type.GetConstructor(Type.EmptyTypes) != null;
+            return type.GetConstructor(BindingFlags, Type.EmptyTypes) != null;
 
         if (type.IsValueType)
-            return type.GetConstructors().Length == 0 || type.GetConstructor(Type.EmptyTypes) != null;
+            return type.GetConstructors(BindingFlags).Length == 0 || type.GetConstructor(BindingFlags, Type.EmptyTypes) != null;
 
         throw new ArgumentException($"{type} is not constructable");
     }
