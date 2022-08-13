88fa284b5d2e65bd78ef53cdfeed97306e1d9409
Alex Kreskiyan <a.kreskiyan@gmail.com>
2022-08-13 22:49:17 +0300

    Blazor.Css: add CursorExtensions

---
+++ client/Blazor/src/Annium.Blazor.Css/Extensions/CursorExtensions.cs
@@ -0,0 +1,25 @@
+namespace Annium.Blazor.Css;
+
+public static class CursorExtensions
+{
+    public static CssRule CursorCrosshair(this CssRule rule) =>
+        rule.Cursor("crosshair");
+
+    public static CssRule CursorHelp(this CssRule rule) =>
+        rule.Cursor("help");
+
+    public static CssRule CursorMove(this CssRule rule) =>
+        rule.Cursor("move");
+
+    public static CssRule CursorPointer(this CssRule rule) =>
+        rule.Cursor("pointer");
+
+    public static CssRule CursorText(this CssRule rule) =>
+        rule.Cursor("text");
+
+    public static CssRule CursorWait(this CssRule rule) =>
+        rule.Cursor("wait");
+
+    private static CssRule Cursor(this CssRule rule, string cursor) =>
+        rule.Set("cursor", cursor);
+}
