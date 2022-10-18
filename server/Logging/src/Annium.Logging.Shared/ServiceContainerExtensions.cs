f623495cf39593b7ffd89e8f0fc57a00745e2b3e
Alex Kreskiyan <a.kreskiyan@gmail.com>
2022-10-18 17:48:25 +0600

    Logging.Shared: make always scoped

--- server/Logging/src/Annium.Logging.Shared/ServiceContainerExtensions.cs
+++ server/Logging/src/Annium.Logging.Shared/ServiceContainerExtensions.cs
@@ -13,31 +13,30 @@ public static class ServiceContainerExtensions
     )
         where TContext : class, ILogContext
     {
-        return container.AddLoggingBase<TContext>(ServiceLifetime.Scoped);
+        return container.AddLoggingBase<TContext>();
     }
 
     public static IServiceContainer AddLogging(
         this IServiceContainer container
     )
     {
-        return container.AddLoggingBase<DefaultLogContext>(ServiceLifetime.Singleton);
+        return container.AddLoggingBase<DefaultLogContext>();
     }
 
     private static IServiceContainer AddLoggingBase<TContext>(
-        this IServiceContainer container,
-        ServiceLifetime lifetime
+        this IServiceContainer container
     )
         where TContext : class, ILogContext
     {
         container.Add(new List<ILogScheduler<TContext>>()).AsSelf().As<IReadOnlyCollection<ILogScheduler<TContext>>>().Singleton();
 
-        container.Add<TContext>().AsSelf().In(lifetime);
-        container.Add(typeof(Logger<>)).As(typeof(ILogger<>)).In(lifetime);
-        container.Add(typeof(LogSubject<>)).As(typeof(ILogSubject<>)).In(lifetime);
-        container.Add<ILoggerFactory, LoggerFactory>().In(lifetime);
+        container.Add<TContext>().AsSelf().In(ServiceLifetime.Scoped);
+        container.Add(typeof(Logger<>)).As(typeof(ILogger<>)).In(ServiceLifetime.Scoped);
+        container.Add(typeof(LogSubject<>)).As(typeof(ILogSubject<>)).In(ServiceLifetime.Scoped);
+        container.Add<ILoggerFactory, LoggerFactory>().In(ServiceLifetime.Scoped);
         container.Add<LogRouter<TContext>>().AsSelf().Singleton();
         container.Add<ILogSentry<TContext>, LogSentry<TContext>>().Singleton();
-        container.Add<ILogSentryBridge, LogSentryBridge<TContext>>().In(lifetime);
+        container.Add<ILogSentryBridge, LogSentryBridge<TContext>>().In(ServiceLifetime.Scoped);
         container.AddProfile(p =>
         {
             p.Map<LogLevel, string>(x => x.ToString());
