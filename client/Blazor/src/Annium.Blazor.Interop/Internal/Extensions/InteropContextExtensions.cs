d382e3e0617b8ad67548a9de32f47976a64ee196
Alex Kreskiyan <a.kreskiyan@gmail.com>
2022-12-31 18:50:57 +0300

    Blazor.Interop: cleanup unused unmarshalled runtime invocations

--- client/Blazor/src/Annium.Blazor.Interop/Internal/Extensions/InteropContextExtensions.cs
+++ client/Blazor/src/Annium.Blazor.Interop/Internal/Extensions/InteropContextExtensions.cs
@@ -27,10 +27,6 @@ public static class InteropContextExtensions
         => ctx.UnmarshalledRuntime.InvokeUnmarshalled<T>(Helper.Call(identifier));
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void Invoke(this IInteropContext ctx, string identifier)
-        => ctx.UnmarshalledRuntime.InvokeUnmarshalled<object>(Helper.Call(identifier));
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static T Invoke<T1, T>(this IInteropContext ctx, string identifier, T1 x1)
         => ctx.UnmarshalledRuntime.InvokeUnmarshalled<T1, T>(Helper.Call(identifier), x1);
 
@@ -47,40 +43,12 @@ public static class InteropContextExtensions
         => ctx.UnmarshalledRuntime.InvokeUnmarshalled<ValueTuple<T1, T2>, object>(Helper.Call(identifier), ValueTuple.Create(x1, x2));
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static T UInvoke<T1, T2, T3, T>(this IInteropContext ctx, string identifier, T1 x1, T2 x2, T3 x3)
-        => ctx.UnmarshalledRuntime.InvokeUnmarshalled<ValueTuple<T1, T2, T3>, T>(Helper.Call(identifier), ValueTuple.Create(x1, x2, x3));
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void Invoke<T1, T2, T3>(this IInteropContext ctx, string identifier, T1 x1, T2 x2, T3 x3)
         => ctx.UnmarshalledRuntime.InvokeUnmarshalled<ValueTuple<T1, T2, T3>, object>(Helper.Call(identifier), ValueTuple.Create(x1, x2, x3));
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static T Invoke<T1, T2, T3, T4, T>(this IInteropContext ctx, string identifier, T1 x1, T2 x2, T3 x3, T4 x4)
-        => ctx.UnmarshalledRuntime.InvokeUnmarshalled<ValueTuple<T1, T2, T3, T4>, T>(Helper.Call(identifier), ValueTuple.Create(x1, x2, x3, x4));
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void Invoke<T1, T2, T3, T4>(this IInteropContext ctx, string identifier, T1 x1, T2 x2, T3 x3, T4 x4)
-        => ctx.UnmarshalledRuntime.InvokeUnmarshalled<ValueTuple<T1, T2, T3, T4>, object>(Helper.Call(identifier), ValueTuple.Create(x1, x2, x3, x4));
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static T Invoke<T1, T2, T3, T4, T5, T>(this IInteropContext ctx, string identifier, T1 x1, T2 x2, T3 x3, T4 x4, T5 x5)
-        => ctx.UnmarshalledRuntime.InvokeUnmarshalled<ValueTuple<T1, T2, T3, T4, T5>, T>(Helper.Call(identifier), ValueTuple.Create(x1, x2, x3, x4, x5));
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void Invoke<T1, T2, T3, T4, T5>(this IInteropContext ctx, string identifier, T1 x1, T2 x2, T3 x3, T4 x4, T5 x5)
         => ctx.UnmarshalledRuntime.InvokeUnmarshalled<ValueTuple<T1, T2, T3, T4, T5>, object>(Helper.Call(identifier), ValueTuple.Create(x1, x2, x3, x4, x5));
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static T Invoke<T1, T2, T3, T4, T5, T6, T>(this IInteropContext ctx, string identifier, T1 x1, T2 x2, T3 x3, T4 x4, T5 x5, T6 x6)
-        => ctx.UnmarshalledRuntime.InvokeUnmarshalled<ValueTuple<T1, T2, T3, T4, T5, T6>, T>(Helper.Call(identifier), ValueTuple.Create(x1, x2, x3, x4, x5, x6));
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void Invoke<T1, T2, T3, T4, T5, T6>(this IInteropContext ctx, string identifier, T1 x1, T2 x2, T3 x3, T4 x4, T5 x5, T6 x6)
-        => ctx.UnmarshalledRuntime.InvokeUnmarshalled<ValueTuple<T1, T2, T3, T4, T5, T6>, object>(Helper.Call(identifier), ValueTuple.Create(x1, x2, x3, x4, x5, x6));
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static T Invoke<T1, T2, T3, T4, T5, T6, T7, T>(this IInteropContext ctx, string identifier, T1 x1, T2 x2, T3 x3, T4 x4, T5 x5, T6 x6, T7 x7)
-        => ctx.UnmarshalledRuntime.InvokeUnmarshalled<ValueTuple<T1, T2, T3, T4, T5, T6, T7>, T>(Helper.Call(identifier), ValueTuple.Create(x1, x2, x3, x4, x5, x6, x7));
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void Invoke<T1, T2, T3, T4, T5, T6, T7>(this IInteropContext ctx, string identifier, T1 x1, T2 x2, T3 x3, T4 x4, T5 x5, T6 x6, T7 x7)
