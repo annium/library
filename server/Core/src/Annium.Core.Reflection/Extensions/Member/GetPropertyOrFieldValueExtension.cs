2e4a79c826af34ac3b47144d0e50f24fb1df18a4
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-01-15 20:41:53 +0300

    Core.Reflection: fix GetPropertyOrFieldValueExtension

--- server/Core/src/Annium.Core.Reflection/Extensions/Member/GetPropertyOrFieldValueExtension.cs
+++ server/Core/src/Annium.Core.Reflection/Extensions/Member/GetPropertyOrFieldValueExtension.cs
@@ -7,8 +7,7 @@ public static class GetPropertyOrFieldValueExtension
 {
     public static object? GetPropertyOrFieldValue(this MemberInfo member, object target) => member switch
     {
-        PropertyInfo property => property.GetGetMethod()?.Invoke(target, Array.Empty<object>())
-            ?? throw new InvalidOperationException($"property {member} is not readable"),
+        PropertyInfo property => property.GetGetMethod()?.Invoke(target, Array.Empty<object>()),
         FieldInfo field => field.GetValue(target),
         _               => throw new InvalidOperationException($"{member} is neither readable property nor field")
     };
