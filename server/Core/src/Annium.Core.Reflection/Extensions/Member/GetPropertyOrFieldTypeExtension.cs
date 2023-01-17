e08772738bea4c1012c3433176a06da814e0cff3
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-01-17 13:23:16 +0300

    Core.Reflection: fix GetPropertyOrFieldValueExtension, add GetPropertyOrFieldTypeExtension

---
+++ server/Core/src/Annium.Core.Reflection/Extensions/Member/GetPropertyOrFieldTypeExtension.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Reflection;
+
+namespace Annium.Core.Reflection;
+
+public static class GetPropertyOrFieldTypeExtension
+{
+    public static Type GetPropertyOrFieldType(this MemberInfo member) => member switch
+    {
+        PropertyInfo property => property.PropertyType,
+        FieldInfo field       => field.FieldType,
+        _                     => throw new InvalidOperationException($"{member} is neither readable property nor field")
+    };
+}
--- server/Core/src/Annium.Core.Reflection/Extensions/Member/GetPropertyOrFieldValueExtension.cs
+++ server/Core/src/Annium.Core.Reflection/Extensions/Member/GetPropertyOrFieldValueExtension.cs
@@ -7,8 +7,8 @@ public static class GetPropertyOrFieldValueExtension
 {
     public static object? GetPropertyOrFieldValue(this MemberInfo member, object target) => member switch
     {
-        PropertyInfo property => property.GetGetMethod()?.Invoke(target, Array.Empty<object>()),
-        FieldInfo field => field.GetValue(target),
-        _               => throw new InvalidOperationException($"{member} is neither readable property nor field")
+        PropertyInfo property => property.GetMethod?.Invoke(target, Array.Empty<object>()),
+        FieldInfo field       => field.GetValue(target),
+        _                     => throw new InvalidOperationException($"{member} is neither readable property nor field")
     };
 }
