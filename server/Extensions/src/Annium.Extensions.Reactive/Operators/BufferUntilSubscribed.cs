a5987233e14caacdbe5a8ddffbf093d0ab09cc92
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-01-27 09:56:08 +0300

    Extensions.Reactive: use file-local types

--- server/Extensions/src/Annium.Extensions.Reactive/Operators/BufferUntilSubscribed.cs
+++ server/Extensions/src/Annium.Extensions.Reactive/Operators/BufferUntilSubscribed.cs
@@ -21,166 +21,163 @@ public static class BufferUntilSubscribedOperatorExtensions
 
         ctx.Trace("create observable");
 
-        return Observable.Create(CreateObservable(ctx));
+        return Observable.Create<T>(observer => ctx.IsFlushed ? ctx.Subscribe(observer) : ctx.SubscribeFirst(observer));
+    }
+}
+
+file record BufferContext<T>
+{
+    public bool IsFlushed { get; private set; }
+    public CancellationToken DataCt => _dataCts.Token;
+    public CancellationToken CompletionCt => _completionCts.Token;
+    private readonly IObservable<T> _source;
+    private readonly Queue<ObservableEvent<T>> _events = new();
+    private readonly CancellationTokenSource _dataCts;
+    private readonly CancellationTokenSource _completionCts = new();
+    private bool _isCompleted;
+    private IObserver<T>? _firstObserver;
+    private IDisposable _firstSubscription = Disposable.Empty;
+
+    public BufferContext(
+        IObservable<T> source
+    )
+    {
+        _source = source;
+        _dataCts = CancellationTokenSource.CreateLinkedTokenSource(_completionCts.Token);
     }
 
-    private static Func<IObserver<T>, IDisposable> CreateObservable<T>(BufferContext<T> ctx) => observer =>
-        ctx.IsFlushed ? ctx.Subscribe(observer) : ctx.SubscribeFirst(observer);
-
-    private class BufferContext<T>
+    public IDisposable SubscribeFirst(IObserver<T> observer)
     {
-        public bool IsFlushed { get; private set; }
-        public CancellationToken DataCt => _dataCts.Token;
-        public CancellationToken CompletionCt => _completionCts.Token;
-        private readonly IObservable<T> _source;
-        private readonly Queue<ObservableEvent<T>> _events = new();
-        private readonly CancellationTokenSource _dataCts;
-        private readonly CancellationTokenSource _completionCts = new();
-        private bool _isCompleted;
-        private IObserver<T>? _firstObserver;
-        private IDisposable _firstSubscription = Disposable.Empty;
-
-        public BufferContext(
-            IObservable<T> source
-        )
+        lock (this)
         {
-            _source = source;
-            _dataCts = CancellationTokenSource.CreateLinkedTokenSource(_completionCts.Token);
-        }
+            var target = observer.GetFullId();
+            Trace($"{target} - start");
 
-        public IDisposable SubscribeFirst(IObserver<T> observer)
+            Flush(observer);
+            SetFirstObserver(observer);
+            var subscription = Disposable.Create(DisposeFirstSubscription);
+
+            Trace($"{target} - done");
+
+            return subscription;
+        }
+    }
+
+    public IDisposable Subscribe(IObserver<T> observer)
+    {
+        lock (this)
         {
-            lock (this)
-            {
-                var target = observer.GetFullId();
-                Trace($"{target} - start");
+            Trace("start");
+            var subscription = _source.Subscribe(observer);
+            Trace("done");
 
-                Flush(observer);
-                SetFirstObserver(observer);
-                var subscription = Disposable.Create(DisposeFirstSubscription);
-
-                Trace($"{target} - done");
-
-                return subscription;
-            }
+            return subscription;
         }
+    }
 
-        public IDisposable Subscribe(IObserver<T> observer)
-        {
-            lock (this)
-            {
-                Trace("start");
-                var subscription = _source.Subscribe(observer);
-                Trace("done");
-
-                return subscription;
-            }
-        }
-
-        public void Process(T data, Exception? error)
-        {
-            lock (this)
-            {
-                Trace("start");
-
-                if (_isCompleted)
-                    throw new InvalidOperationException($"Can't add: {data} - {error}, ctx already completed");
-
-                var e = new ObservableEvent<T>(data, error, false);
-                if (_firstObserver is null)
-                {
-                    Trace($"enqueue: {data} - {error}");
-                    _events.Enqueue(e);
-                }
-                else
-                {
-                    Trace($"first - handle: {data} - {error}");
-                    _firstObserver.Handle(e);
-                    SetFirstSubscription(Subscribe(_firstObserver));
-                }
-
-                Trace("done");
-            }
-        }
-
-        public void Complete()
-        {
-            lock (this)
-            {
-                Trace("start");
-
-                if (_isCompleted)
-                    throw new InvalidOperationException("source already completed");
-
-                if (!IsFlushed)
-                {
-                    Trace("enqueue: completed");
-                    _events.Enqueue(new ObservableEvent<T>(default!, null, true));
-                }
-
-                _isCompleted = true;
-
-                _firstObserver = null;
-                _completionCts.Cancel();
-
-                Trace("done");
-            }
-        }
-
-        private void Flush(IObserver<T> observer)
+    public void Process(T data, Exception? error)
+    {
+        lock (this)
         {
             Trace("start");
 
-            IsFlushed = true;
-            while (_events.TryDequeue(out var e))
-                observer.Handle(e);
+            if (_isCompleted)
+                throw new InvalidOperationException($"Can't add: {data} - {error}, ctx already completed");
+
+            var e = new ObservableEvent<T>(data, error, false);
+            if (_firstObserver is null)
+            {
+                Trace($"enqueue: {data} - {error}");
+                _events.Enqueue(e);
+            }
+            else
+            {
+                Trace($"first - handle: {data} - {error}");
+                _firstObserver.Handle(e);
+                SetFirstSubscription(Subscribe(_firstObserver));
+            }
 
             Trace("done");
         }
+    }
 
-        private void SetFirstObserver(IObserver<T> observer)
+    public void Complete()
+    {
+        lock (this)
         {
             Trace("start");
 
-            if (_firstObserver is not null)
-                throw new InvalidOperationException("first observer already subscribed");
-            _firstObserver = observer;
+            if (_isCompleted)
+                throw new InvalidOperationException("source already completed");
 
-            Trace("done");
-        }
+            if (!IsFlushed)
+            {
+                Trace("enqueue: completed");
+                _events.Enqueue(new ObservableEvent<T>(default!, null, true));
+            }
 
-        private void SetFirstSubscription(IDisposable subscription)
-        {
-            Trace("start");
+            _isCompleted = true;
 
-            if (!ReferenceEquals(_firstSubscription, Disposable.Empty))
-                throw new InvalidOperationException("first subscription already set");
-
-            _firstSubscription = subscription;
             _firstObserver = null;
-            _dataCts.Cancel();
+            _completionCts.Cancel();
 
             Trace("done");
         }
+    }
 
-        private void DisposeFirstSubscription()
-        {
-            Trace("start");
+    private void Flush(IObserver<T> observer)
+    {
+        Trace("start");
 
-            _firstSubscription.Dispose();
-            _firstSubscription = Disposable.Empty;
+        IsFlushed = true;
+        while (_events.TryDequeue(out var e))
+            observer.Handle(e);
 
-            Trace("done");
-        }
+        Trace("done");
+    }
 
-        public void Trace(
-            string msg,
-            [CallerFilePath] string callerFilePath = "",
-            [CallerMemberName] string member = "",
-            [CallerLineNumber] int line = 0
-        )
-        {
-            this.Trace(msg, false, callerFilePath, member, line);
-        }
+    private void SetFirstObserver(IObserver<T> observer)
+    {
+        Trace("start");
+
+        if (_firstObserver is not null)
+            throw new InvalidOperationException("first observer already subscribed");
+        _firstObserver = observer;
+
+        Trace("done");
+    }
+
+    private void SetFirstSubscription(IDisposable subscription)
+    {
+        Trace("start");
+
+        if (!ReferenceEquals(_firstSubscription, Disposable.Empty))
+            throw new InvalidOperationException("first subscription already set");
+
+        _firstSubscription = subscription;
+        _firstObserver = null;
+        _dataCts.Cancel();
+
+        Trace("done");
+    }
+
+    private void DisposeFirstSubscription()
+    {
+        Trace("start");
+
+        _firstSubscription.Dispose();
+        _firstSubscription = Disposable.Empty;
+
+        Trace("done");
+    }
+
+    public void Trace(
+        string msg,
+        [CallerFilePath] string callerFilePath = "",
+        [CallerMemberName] string member = "",
+        [CallerLineNumber] int line = 0
+    )
+    {
+        this.Trace(msg, false, callerFilePath, member, line);
     }
 }
--- server/Extensions/src/Annium.Extensions.Reactive/Operators/TrackCompletion.cs
+++ server/Extensions/src/Annium.Extensions.Reactive/Operators/TrackCompletion.cs
@@ -19,91 +19,89 @@ public static class TrackCompletionOperatorExtensions
 
         ctx.Trace("create observable");
 
-        return Observable.Create(CreateObservable(ctx));
+        return Observable.Create<T>(observer =>
+        {
+            var target = observer.GetFullId();
+            ctx.Trace($"{target} - handle");
+
+            if (!ctx.IsCompleted)
+                return ctx.Subscribe(observer);
+
+            ctx.Trace($"{target} - complete");
+            observer.OnCompleted();
+            ctx.Trace($"{target} - completed");
+
+            return Disposable.Empty;
+        });
+    }
+}
+
+file record CompletionContext<T>
+{
+    public bool IsCompleted { get; private set; }
+    public CancellationToken CompletionCt => _completionCts.Token;
+    private readonly IObservable<T> _source;
+    private readonly List<IObserver<T>> _incompleteObservers = new();
+    private readonly CancellationTokenSource _completionCts = new();
+
+    public CompletionContext(
+        IObservable<T> source
+    )
+    {
+        _source = source;
     }
 
-    private static Func<IObserver<T>, IDisposable> CreateObservable<T>(CompletionContext<T> ctx) => observer =>
+    public IDisposable Subscribe(IObserver<T> observer)
     {
-        var target = observer.GetFullId();
-        ctx.Trace($"{target} - handle");
-
-        if (!ctx.IsCompleted)
-            return ctx.Subscribe(observer);
-
-        ctx.Trace($"{target} - complete");
-        observer.OnCompleted();
-        ctx.Trace($"{target} - completed");
-
-        return Disposable.Empty;
-    };
-
-    private class CompletionContext<T>
-    {
-        public bool IsCompleted { get; private set; }
-        public CancellationToken CompletionCt => _completionCts.Token;
-        private readonly IObservable<T> _source;
-        private readonly List<IObserver<T>> _incompleteObservers = new();
-        private readonly CancellationTokenSource _completionCts = new();
-
-        public CompletionContext(
-            IObservable<T> source
-        )
-        {
-            _source = source;
-        }
-
-        public IDisposable Subscribe(IObserver<T> observer)
-        {
-            lock (this)
-            {
-                Trace("start");
-
-                _incompleteObservers.Add(observer);
-                var subscription = _source.Subscribe(observer.OnNext, observer.OnError);
-
-                Trace("done");
-
-                return subscription;
-            }
-        }
-
-        public void Complete()
+        lock (this)
         {
             Trace("start");
 
-            IReadOnlyCollection<IObserver<T>> observers;
-            lock (this)
-            {
-                if (IsCompleted)
-                    throw new InvalidOperationException("source already completed");
-                IsCompleted = true;
-
-                Trace($"complete {_incompleteObservers.Count} observers");
-                observers = _incompleteObservers.ToArray();
-                _incompleteObservers.Clear();
-            }
-
-            foreach (var observer in observers)
-            {
-                Trace($"complete {observer.GetFullId()}");
-                observer.OnCompleted();
-            }
-
-            Trace("cancel cts");
-            _completionCts.Cancel();
+            _incompleteObservers.Add(observer);
+            var subscription = _source.Subscribe(observer.OnNext, observer.OnError);
 
             Trace("done");
+
+            return subscription;
+        }
+    }
+
+    public void Complete()
+    {
+        Trace("start");
+
+        IReadOnlyCollection<IObserver<T>> observers;
+        lock (this)
+        {
+            if (IsCompleted)
+                throw new InvalidOperationException("source already completed");
+            IsCompleted = true;
+
+            Trace($"complete {_incompleteObservers.Count} observers");
+            observers = _incompleteObservers.ToArray();
+            _incompleteObservers.Clear();
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Trace(
-            string msg,
-            [CallerFilePath] string callerFilePath = "",
-            [CallerMemberName] string member = "",
-            [CallerLineNumber] int line = 0
-        )
+        foreach (var observer in observers)
         {
-            this.Trace(msg, false, callerFilePath, member, line);
+            Trace($"complete {observer.GetFullId()}");
+            observer.OnCompleted();
         }
+
+        Trace("cancel cts");
+        _completionCts.Cancel();
+
+        Trace("done");
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Trace(
+        string msg,
+        [CallerFilePath] string callerFilePath = "",
+        [CallerMemberName] string member = "",
+        [CallerLineNumber] int line = 0
+    )
+    {
+        this.Trace(msg, false, callerFilePath, member, line);
     }
 }
