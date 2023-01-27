b57c4d780a7e877784c2f198c908146de5c7c614
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-01-27 10:48:29 +0300

    Core.Internal: update FriendlyName implementation

--- server/Core/src/Annium.Core.Internal/LogExtensions.cs
+++ server/Core/src/Annium.Core.Internal/LogExtensions.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.CompilerServices;
+using Annium.Core.Internal.Internal;
 using Annium.Diagnostics.Debug;
 
 namespace Annium.Core.Internal;
@@ -25,44 +23,4 @@ public static class LogExtensions
         var msg = string.IsNullOrWhiteSpace(message) ? string.Empty : $" >> {message}";
         Log.Trace(subject, $"{msg}{trace}", callerFilePath, member, line);
     }
-
-    private static string FriendlyName(this Type value)
-    {
-        if (TypeNames.TryGetValue(value, out var name))
-            return name;
-
-        if (value.IsGenericParameter || !value.IsGenericType)
-            return TypeNames[value] = value.Name;
-
-        if (value.GetGenericTypeDefinition() == typeof(Nullable<>))
-            return TypeNames[value] = $"{Nullable.GetUnderlyingType(value)!.FriendlyName()}?";
-
-        name = value.GetGenericTypeDefinition().Name;
-        if (name.IndexOf('`') >= 0)
-            name = name.Substring(0, name.IndexOf('`'));
-        var arguments = value.GetGenericArguments().Select(x => x.FriendlyName()).ToArray();
-
-        return TypeNames.AddOrUpdate(value, $"{name}<{string.Join(", ", arguments)}>", (_, x) => x);
-    }
-
-    private static readonly ConcurrentDictionary<Type, string> TypeNames = new(new Dictionary<Type, string>
-        {
-            { typeof(int), "int" },
-            { typeof(uint), "uint" },
-            { typeof(long), "long" },
-            { typeof(ulong), "ulong" },
-            { typeof(short), "short" },
-            { typeof(ushort), "ushort" },
-            { typeof(byte), "byte" },
-            { typeof(sbyte), "sbyte" },
-            { typeof(bool), "bool" },
-            { typeof(float), "float" },
-            { typeof(double), "double" },
-            { typeof(decimal), "decimal" },
-            { typeof(char), "char" },
-            { typeof(string), "string" },
-            { typeof(object), "object" },
-            { typeof(void), "void" },
-        }
-    );
 }
