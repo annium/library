d23a993c1f7000e4efab0b771de413e4889ac564
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-02-16 18:36:04 +0300

    Net.Types: cleanup IgnoredProcesor

--- server/Net/src/Annium.Net.Types/Internal/Processors/IgnoredProcessor.cs
+++ server/Net/src/Annium.Net.Types/Internal/Processors/IgnoredProcessor.cs
@@ -1,4 +1,3 @@
-using Annium.Net.Types.Internal.Extensions;
 using Namotion.Reflection;
 
 namespace Annium.Net.Types.Internal.Processors;
@@ -7,16 +6,6 @@ internal class IgnoredProcessor : IProcessor
 {
     public bool Process(ContextualType type, Nullability nullability, IProcessingContext ctx)
     {
-        if (!MapperConfig.IsIgnored(type))
-            return false;
-
-        if (!type.Type.IsGenericType)
-            return true;
-
-        var typeGenericArguments = type.GetGenericArguments();
-        foreach (var arg in typeGenericArguments)
-            ctx.Process(arg);
-
-        return true;
+        return MapperConfig.IsIgnored(type);
     }
 }
