e6746d5d5ed890a1ca3a65367a67f764c8f36cbf
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-02-19 02:32:24 +0300

    base: update ReferrerExtensions.BuildRef to be more generic

--- server/Net/src/Annium.Net.Types/Internal/Helpers/ReferrerExtensions.cs
+++ server/Net/src/Annium.Net.Types/Internal/Helpers/ReferrerExtensions.cs
@@ -13,12 +13,10 @@ internal static class ReferrerExtensions
     public static TRef BuildRef<TRef>(this IReferrer referrer, ContextualType type, IProcessingContext ctx, Func<string, string, IRef[], TRef> factory)
         where TRef : IModelRef
     {
-        var pure = type.Type.GetPure().ToContextualType();
-        var baseRef = (TRef) ctx.RequireRef(pure);
         var name = type.PureName();
         var typeGenericArguments = type.GetGenericArguments();
         var genericArguments = typeGenericArguments.Select(ctx.GetRef).ToArray();
-        var modelRef = factory(baseRef.Namespace, name, genericArguments);
+        var modelRef = factory(type.Type.Namespace ?? string.Empty, name, genericArguments);
         referrer.Trace($"Created {type.FriendlyName()} ref as {modelRef}");
 
         return modelRef;
