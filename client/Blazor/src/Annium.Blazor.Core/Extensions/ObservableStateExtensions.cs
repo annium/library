6dd7d66cd03c375f43bdfe0127f039e8c6f0f581
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-03-08 11:07:22 +0300

    Blazor.Core: invert IObservableState notification logic into Notify extension

--- client/Blazor/src/Annium.Blazor.Core/Extensions/ObservableStateExtensions.cs
+++ client/Blazor/src/Annium.Blazor.Core/Extensions/ObservableStateExtensions.cs
@@ -1,21 +1,29 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Annium.Components.State.Core;
 using Microsoft.AspNetCore.Components;
 
 namespace Annium.Blazor.Core.Extensions;
 
-public static class ComponentBaseStateExtensions
+public static class ObservableStateExtensions
 {
     private static readonly MethodInfo StateHasChanged = typeof(ComponentBase)
         .GetMethod("StateHasChanged", BindingFlags.Instance | BindingFlags.NonPublic)!;
 
     private static readonly object[] EmptyArgs = Array.Empty<object>();
 
+    public static IDisposable Notify<T>(this T state, ComponentBase component)
+        where T : IObservableState =>
+        state.Changed.Subscribe(_ => StateHasChanged.Invoke(component, EmptyArgs));
+
+    public static IEnumerable<IDisposable> Notify<T>(this IEnumerable<T> states, ComponentBase component)
+        where T : IObservableState
+    {
+        return states.Select(x => x.Changed.Subscribe(_ => StateHasChanged.Invoke(component, EmptyArgs)));
+    }
+
     public static IDisposable ObserveStates(this ComponentBase component) =>
         State.ObserveObject(component, () => StateHasChanged.Invoke(component, EmptyArgs));
-
-    public static IDisposable ObserveState<T>(this ComponentBase component, T target)
-        where T : IObservableState =>
-        target.Changed.Subscribe(_ => StateHasChanged.Invoke(component, EmptyArgs));
 }
