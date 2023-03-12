867224e90367bf91463771efcd5177b042742630
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-03-12 22:50:42 +0300

    Blazor.Core: extend ObservableStateExtensions.Notify to invoke function

--- client/Blazor/src/Annium.Blazor.Core/Extensions/ObservableStateExtensions.cs
+++ client/Blazor/src/Annium.Blazor.Core/Extensions/ObservableStateExtensions.cs
@@ -14,6 +14,16 @@ public static class ObservableStateExtensions
 
     private static readonly object[] EmptyArgs = Array.Empty<object>();
 
+    public static IDisposable Notify<T>(this T state, Action<T> handle)
+        where T : IObservableState =>
+        state.Changed.Subscribe(_ => handle(state));
+
+    public static IEnumerable<IDisposable> Notify<T>(this IEnumerable<T> states, Action<T> handle)
+        where T : IObservableState
+    {
+        return states.Select(x => x.Changed.Subscribe(_ => handle(x)));
+    }
+
     public static IDisposable Notify<T>(this T state, ComponentBase component)
         where T : IObservableState =>
         state.Changed.Subscribe(_ => StateHasChanged.Invoke(component, EmptyArgs));
