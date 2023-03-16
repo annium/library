31c2b56ab41ed9dd22403a1074fc867f17ee371a
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-03-16 13:24:53 +0300

    Annium: implement CartesianProduct enumerable extension

--- server/Annium/src/Annium/Linq/EnumerableExtensions.cs
+++ server/Annium/src/Annium/Linq/EnumerableExtensions.cs
@@ -62,4 +62,16 @@ public static class EnumerableExtensions
     {
         return new SortedList<TKey, T>(items.ToDictionary(getKey, x => x));
     }
+
+    public static IEnumerable<IEnumerable<T>> CartesianProduct<T>(this IEnumerable<IEnumerable<T>> sequences)
+    {
+        IEnumerable<IEnumerable<T>> emptyProduct = new[] { Enumerable.Empty<T>() };
+
+        return sequences.Aggregate(
+            emptyProduct,
+            (accumulator, sequence) =>
+                from acc in accumulator
+                from item in sequence
+                select acc.Concat(new[] { item }));
+    }
 }
