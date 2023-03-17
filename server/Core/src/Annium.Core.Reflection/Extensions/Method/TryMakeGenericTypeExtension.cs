11f5e344ad89a50e4f331b0ffcb5d2611afec32a
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-03-17 19:19:14 +0300

    Core.Reflection: implement TryMakeGenericTypeExtension

---
+++ server/Core/src/Annium.Core.Reflection/Extensions/Method/TryMakeGenericTypeExtension.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Reflection;
+
+// ReSharper disable once CheckNamespace
+namespace Annium.Core.Reflection;
+
+public static class TryMakeGenericMethodExtension
+{
+    public static bool TryMakeGenericMethod(this MethodInfo method, out MethodInfo? result, params Type[] typeArguments)
+    {
+        if (method is null)
+            throw new ArgumentNullException(nameof(method));
+
+        try
+        {
+            result = method.MakeGenericMethod(typeArguments);
+
+            return true;
+        }
+        catch
+        {
+            result = null;
+
+            return false;
+        }
+    }
+}
