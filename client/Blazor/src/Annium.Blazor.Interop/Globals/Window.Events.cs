ac0115181d92df37d2a4b9a2e4d2f49ccc3115b0
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-06-02 21:31:49 +0300

    Blazor.Interop: add Window keyboard events

--- client/Blazor/src/Annium.Blazor.Interop/Globals/Window.Events.cs
+++ client/Blazor/src/Annium.Blazor.Interop/Globals/Window.Events.cs
@@ -2,11 +2,22 @@ using System;
 using Annium.Blazor.Interop.Domain;
 using Annium.Blazor.Interop.Internal;
 
+// ReSharper disable once CheckNamespace
 namespace Annium.Blazor.Interop;
 
 public static partial class Window
 {
-    private static readonly IInteropEvent<ResizeEvent> ResizeEvent = InteropEvent<ResizeEvent>.Static("window", "window");
+    private static readonly IInteropEvent<KeyboardEvent> KeyboardEvent =
+        InteropEvent<KeyboardEvent>.Static("window", "window");
+
+    private static readonly IInteropEvent<ResizeEvent> ResizeEvent =
+        InteropEvent<ResizeEvent>.Static("window", "window");
+
+    public static Action OnKeyDown(Action<KeyboardEvent> handle, bool preventDefault) =>
+        KeyboardEvent.Register(KeyboardEventEnum.keydown, handle, preventDefault);
+
+    public static Action OnKeyUp(Action<KeyboardEvent> handle, bool preventDefault) =>
+        KeyboardEvent.Register(KeyboardEventEnum.keyup, handle, preventDefault);
 
     public static Action OnResize(Action<ResizeEvent> handle) => ResizeEvent.Register("resize", handle);
 }
--- client/Blazor/src/Annium.Blazor.Interop/wwwroot/globals/window.ts
+++ client/Blazor/src/Annium.Blazor.Interop/wwwroot/globals/window.ts
@@ -1,5 +1,18 @@
 import cbTracker from '../trackers/cbTracker.js';
-import { getLog } from '../log.js';
+import {getLog} from '../log.js';
+
+type KeyboardEventName =
+  | 'keydown'
+  | 'keyup'
+
+type MouseEventName =
+  | 'mousedown'
+  | 'mouseenter'
+  | 'mouseleave'
+  | 'mousemove'
+  | 'mouseout'
+  | 'mouseover'
+  | 'mouseup'
 
 const log = getLog('window')
 
@@ -9,17 +22,50 @@ export const innerWidth = (): number => window.innerWidth
 export const innerHeight = (): number => window.innerHeight
 
 /* methods */
+export const onKeyboardEvent = (type: KeyboardEventName, ref: DotNet.DotNetObject, method: string, preventDefault: boolean): number => {
+  const callback = cbTracker.track(preventDefault
+    ? (e: KeyboardEvent) => {
+      e.preventDefault();
+      ref.invokeMethod(method, callback.id, [e.key, e.code, e.metaKey, e.ctrlKey, e.altKey, e.shiftKey])
+    }
+    : (e: KeyboardEvent) => {
+      ref.invokeMethod(method, callback.id, [e.key, e.code, e.metaKey, e.ctrlKey, e.altKey, e.shiftKey])
+    })
+  log.debug('window', 'onKeyboardEvent', type, 'add callback', callback.id)
+  window.addEventListener(type, callback)
+
+  return callback.id
+}
+export const offKeyboardEvent = (type: KeyboardEventName, cid: number): void => {
+  log.debug('window', 'offKeyboardEvent', type, 'release callback', cid)
+  window.removeEventListener(type, cbTracker.release(cid))
+}
+
+export const onMouseEvent = (type: MouseEventName, ref: DotNet.DotNetObject, method: string): number => {
+  const callback = cbTracker.track((e: MouseEvent) => {
+    e.preventDefault();
+    ref.invokeMethod(method, callback.id, [e.clientX, e.clientY, e.metaKey, e.ctrlKey, e.altKey, e.shiftKey])
+  })
+  log.debug('window', 'onMouseEvent', type, 'add callback', callback.id)
+  window.addEventListener(type, callback)
+
+  return callback.id
+}
+export const offMouseEvent = (type: MouseEventName, cid: number): void => {
+  log.debug('window', 'offMouseEvent', type, 'release callback', cid)
+  window.removeEventListener(type, cbTracker.release(cid))
+}
 
 export const onResizeEvent = (type: 'resize', ref: DotNet.DotNetObject, method: string): number => {
   const callback = cbTracker.track((_: UIEvent) => {
     ref.invokeMethod(method, callback.id, [window.innerWidth, window.innerHeight])
   })
-  log.debug('onResizeEvent', 'add callback', callback.id)
+  log.debug('window', 'onResizeEvent', 'add callback', callback.id)
   window.addEventListener(type, callback)
 
   return callback.id
 }
 export const offResizeEvent = (type: 'resize', cid: number): void => {
-  log.debug('offEvent', type, 'release callback', cid)
+  log.debug('window', 'offEvent', type, 'release callback', cid)
   window.removeEventListener(type, cbTracker.release(cid))
 }
