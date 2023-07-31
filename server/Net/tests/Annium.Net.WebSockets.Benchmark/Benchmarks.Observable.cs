9e2366eeafda186782eb9d13aec4266cab6bff50
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-07-31 23:57:03 +0300

    Net.WebSockets: split Plain/Observable benchmarks

---
+++ server/Net/tests/Annium.Net.WebSockets.Benchmark/Benchmarks.Observable.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace Annium.Net.WebSockets.Benchmark;
+
+public partial class Benchmarks
+{
+    private CancellationTokenSource _observableCts = default!;
+    private ManualResetEventSlim _observableGate = default!;
+    private long _observableEventCount;
+    private System.Net.WebSockets.ClientWebSocket _observableSocket = default!;
+    private Task<WebSocketCloseStatus> _observableListenTask = default!;
+
+    [IterationSetup(Target = nameof(Observable))]
+    public void IterationSetup_Observable()
+    {
+        _observableCts = new();
+        _observableGate = new ManualResetEventSlim();
+        _observableEventCount = Constants.TotalMessages;
+
+        _observableSocket = new System.Net.WebSockets.ClientWebSocket();
+        var client = new ManagedWebSocket(_observableSocket);
+        client.TextReceived += HandleMessage_Observable;
+        _observableSocket.ConnectAsync(new Uri($"ws://127.0.0.1:{Constants.Port}/"), CancellationToken.None).GetAwaiter().GetResult();
+        _observableListenTask = client.ListenAsync(_observableCts.Token);
+    }
+
+    [IterationCleanup(Target = nameof(Observable))]
+    public void IterationCleanup_Observable()
+    {
+        _observableSocket.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+        _observableCts.Cancel();
+        _observableListenTask.Wait();
+    }
+
+    [Benchmark]
+    public void Observable()
+    {
+        _observableGate.Wait();
+    }
+
+    private void HandleMessage_Observable(ReadOnlySpan<byte> data)
+    {
+        if (Interlocked.Decrement(ref _observableEventCount) > 0)
+        {
+            return;
+        }
+
+        _observableGate.Set();
+    }
+}
---
+++ server/Net/tests/Annium.Net.WebSockets.Benchmark/Benchmarks.Plain.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace Annium.Net.WebSockets.Benchmark;
+
+[MemoryDiagnoser]
+public partial class Benchmarks
+{
+    private CancellationTokenSource _plainCts = default!;
+    private ManualResetEventSlim _plainGate = default!;
+    private long _plainEventCount;
+    private System.Net.WebSockets.ClientWebSocket _plainSocket = default!;
+    private Task<WebSocketCloseStatus> _plainListenTask = default!;
+
+    [IterationSetup(Target = nameof(Plain))]
+    public void IterationSetup_Plain()
+    {
+        _plainCts = new();
+        _plainGate = new ManualResetEventSlim();
+        _plainEventCount = Constants.TotalMessages;
+
+        _plainSocket = new System.Net.WebSockets.ClientWebSocket();
+        var client = new ManagedWebSocket(_plainSocket);
+        client.TextReceived += HandleMessage_Plain;
+        _plainSocket.ConnectAsync(new Uri($"ws://127.0.0.1:{Constants.Port}/"), CancellationToken.None).GetAwaiter().GetResult();
+        _plainListenTask = client.ListenAsync(_plainCts.Token);
+    }
+
+    [IterationCleanup(Target = nameof(Plain))]
+    public void IterationCleanup_Plain()
+    {
+        _plainSocket.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+        _plainCts.Cancel();
+        _plainListenTask.Wait();
+    }
+
+    [Benchmark]
+    public void Plain()
+    {
+        _plainGate.Wait();
+    }
+
+    private void HandleMessage_Plain(ReadOnlySpan<byte> data)
+    {
+        if (Interlocked.Decrement(ref _plainEventCount) > 0)
+        {
+            return;
+        }
+
+        _plainGate.Set();
+    }
+}
