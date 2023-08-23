117ddd850b492697fcf1abfc4e22799ce11d8120
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-08-24 00:47:37 +0300

    WIP

--- server/Net/src/Annium.Net.WebSockets/ClientWebSocket.cs
+++ server/Net/src/Annium.Net.WebSockets/ClientWebSocket.cs
@@ -1,6 +1,37 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Annium.Net.WebSockets.Internal;
+using NativeWebSocket = System.Net.WebSockets.WebSocket;
+
 namespace Annium.Net.WebSockets;
 
-public class ClientWebSocket
+public class ClientWebSocket : IClientWebSocket
 {
-    
+    public event Action<ReadOnlyMemory<byte>> TextReceived = delegate { };
+    public event Action<ReadOnlyMemory<byte>> BinaryReceived = delegate { };
+    public event Action OnConnected = delegate { };
+    public event Action<WebSocketCloseStatus> OnDisconnected = delegate { };
+    public event Action<Exception> OnError = delegate { };
+    private readonly IClientManagedWebSocket _managedSocket = new ClientManagedWebSocket();
+
+    public void Connect(Uri uri)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Disconnect()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<WebSocketSendStatus> SendTextAsync(ReadOnlyMemory<byte> text, CancellationToken ct = default)
+    {
+        return _managedSocket.SendTextAsync(text, ct);
+    }
+
+    public ValueTask<WebSocketSendStatus> SendBinaryAsync(ReadOnlyMemory<byte> data, CancellationToken ct = default)
+    {
+        return _managedSocket.SendBinaryAsync(data, ct);
+    }
 }
