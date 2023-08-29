61ff1e1719b2b1cba4eb4b0023d57d40d546f4c6
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-08-30 01:00:44 +0300

    Logging.Shared: fix subject id

--- server/Logging/src/Annium.Logging.Shared/Internal/Logger.cs
+++ server/Logging/src/Annium.Logging.Shared/Internal/Logger.cs
@@ -16,11 +16,11 @@ internal class Logger : ILogger
 
     public void Log(object subject, string file, string member, int line, LogLevel level, string message, IReadOnlyList<object?> data)
     {
-        _sentryBridge.Register(subject.GetType().FriendlyName(), subject.GetFullId(), file, member, line, level, message, null, data);
+        _sentryBridge.Register(subject.GetType().FriendlyName(), subject.GetId(), file, member, line, level, message, null, data);
     }
 
     public void Error(object subject, string file, string member, int line, Exception exception, IReadOnlyList<object?> data)
     {
-        _sentryBridge.Register(subject.GetType().FriendlyName(), subject.GetFullId(), file, member, line, LogLevel.Error, exception.Message, exception, data);
+        _sentryBridge.Register(subject.GetType().FriendlyName(), subject.GetId(), file, member, line, LogLevel.Error, exception.Message, exception, data);
     }
 }
