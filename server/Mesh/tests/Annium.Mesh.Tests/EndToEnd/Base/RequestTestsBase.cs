27de4197fd04e57814712394f389ffce7cc6a510
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-10-15 19:22:58 +0300

    Mesh.Tests: init perf tests

--- server/Mesh/tests/Annium.Mesh.Tests/EndToEnd/Base/RequestTestsBase.cs
+++ server/Mesh/tests/Annium.Mesh.Tests/EndToEnd/Base/RequestTestsBase.cs
@@ -1,5 +1,9 @@
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Annium.Architecture.Base;
+using Annium.Data.Operations;
 using Annium.Logging;
 using Annium.Mesh.Tests.System.Domain;
 using Annium.Testing;
@@ -32,6 +36,37 @@ public abstract class RequestTestsBase<TBehavior> : TestBase<TBehavior>
         this.Trace("validate response");
         response.Status.Is(OperationStatus.Ok);
         response.Data.Is(message);
+
+        this.Trace("done");
+    }
+
+    protected async Task Echo_Bundle_Base()
+    {
+        this.Trace("start");
+
+        // arrange
+        this.Trace("get client");
+        await using var client = await GetClient();
+        var responses = new ConcurrentBag<string>();
+        var range = Enumerable.Range(0, 500).Select(x => x.ToString()).ToArray();
+
+        // act
+        await Task.WhenAll(
+            range
+                .Select(async x =>
+                {
+                    this.Trace("send request");
+                    var response = await client.Demo.EchoAsync(new EchoRequest(x)).GetData();
+                    this.Trace("add response");
+                    responses.Add(response);
+                })
+        );
+
+        // assert
+        var set = new HashSet<string>(responses);
+        set.Has(range.Length);
+        foreach (var x in range)
+            set.Contains(x).IsTrue();
 
         this.Trace("done");
     }
---
+++ server/Mesh/tests/Annium.Mesh.Tests/EndToEnd/InMemory/RequestPerfTests.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Annium.Logging;
+using Annium.Mesh.Tests.EndToEnd.Base;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Annium.Mesh.Tests.EndToEnd.InMemory;
+
+public class RequestPerfTests : RequestTestsBase<Behavior>
+{
+    public RequestPerfTests(ITestOutputHelper outputHelper)
+        : base(outputHelper)
+    {
+    }
+
+    [Theory]
+    [MemberData(nameof(GetPerfRange))]
+    public async Task Echo(int index)
+    {
+        this.Trace("start {index}", index);
+
+        await Echo_Base();
+
+        this.Trace("done {index}", index);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetPerfRange))]
+    public async Task Echo_Bundle(int index)
+    {
+        this.Trace("start {index}", index);
+
+        await Echo_Bundle_Base();
+
+        this.Trace("done {index}", index);
+    }
+
+    public static IEnumerable<object[]> GetPerfRange() => Enumerable.Range(0, 10).Select(x => new object[] { x });
+}
