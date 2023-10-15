7150643b109bac7d852f4fc3ef9b3dc69aed02c5
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-10-15 23:42:23 +0300

    Mesh.Tests.WebSockets: init

---
+++ server/Mesh/tests/Annium.Mesh.Tests.WebSockets/Behavior.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Annium.Core.DependencyInjection;
+using Annium.Logging;
+using Annium.Mesh.Server.Web;
+using Annium.Mesh.Tests.Base;
+using Annium.Mesh.Tests.System.Client;
+using Annium.Mesh.Transport.WebSockets;
+using Annium.Net.Servers.Web;
+
+namespace Annium.Mesh.Tests.WebSockets;
+
+public class Behavior : IBehavior, ILogSubject
+{
+    public ILogger Logger { get; }
+    private static int _basePort = 10000;
+
+    public static void Register(IServiceContainer container)
+    {
+        container.Add(new TransportConfiguration(Interlocked.Increment(ref _basePort))).AsSelf().Singleton();
+
+        container.AddMeshWebSocketsClientTransport(sp => new ClientTransportConfiguration
+        {
+            Uri = new Uri($"ws://127.0.0.1:{sp.Resolve<TransportConfiguration>().Port}")
+        });
+        container.AddMeshWebSocketsServerTransport(_ => new ServerTransportConfiguration());
+        container.AddSocketServerMeshHandler();
+
+        container.AddTestServerClient(x => x.WithResponseTimeout(6000));
+    }
+
+    private readonly IServiceProvider _sp;
+    private readonly TransportConfiguration _config;
+
+    public Behavior(
+        IServiceProvider sp,
+        TransportConfiguration config,
+        ILogger logger
+    )
+    {
+        Logger = logger;
+        _sp = sp;
+        _config = config;
+    }
+
+    public Task RunServer(CancellationToken ct)
+    {
+        this.Trace("start, bind server to port {port}", _config.Port);
+        var server = ServerBuilder.New(_sp, _config.Port).WithMeshHandler().Build();
+
+        this.Trace("run server");
+        var runTask = server.RunAsync(ct);
+
+        this.Trace("done, server running");
+
+        return runTask;
+    }
+
+    public record TransportConfiguration(int Port);
+}
---
+++ server/Mesh/tests/Annium.Mesh.Tests.WebSockets/SubscriptionPerfTests.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Annium.Logging;
+using Annium.Mesh.Tests.Base;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Annium.Mesh.Tests.WebSockets;
+
+public class SubscriptionPerfTests : SubscriptionTestsBase<Behavior>
+{
+    public SubscriptionPerfTests(ITestOutputHelper outputHelper)
+        : base(outputHelper)
+    {
+    }
+
+    [Theory]
+    [MemberData(nameof(GetPerfRange))]
+    public async Task Subscription(int index)
+    {
+        this.Trace("start {index}", index);
+
+        await Subscription_Base();
+
+        this.Trace("done {index}", index);
+    }
+
+    public static IEnumerable<object[]> GetPerfRange() => Enumerable.Range(0, 10).Select(x => new object[] { x });
+}
