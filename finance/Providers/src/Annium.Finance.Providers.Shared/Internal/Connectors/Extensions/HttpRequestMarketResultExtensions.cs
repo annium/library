aff1fbd03ca088f399a64c88d8ba291d4923c8a0
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-11-05 16:38:08 +0300

    shared: add http MarketResult/UserResult extensions

---
+++ finance/Providers/src/Annium.Finance.Providers.Shared/Internal/Connectors/Extensions/HttpRequestMarketResultExtensions.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Annium.Finance.Providers.Abstractions.Domain.Operations;
+using Annium.Net.Http;
+
+namespace Annium.Finance.Providers.Shared.Internal.Connectors.Extensions;
+
+internal static class HttpRequestResultExtensions
+{
+    public static async Task<MarketResult<TData>> AsMarketResultAsync<TData, TError>(
+        this IHttpRequest request,
+        TData defaultValue,
+        Func<TError, string> getError
+    )
+    {
+        var response = await request.AsResponseAsync<TData, TError>(defaultValue);
+
+        return response.Data.Match(
+            MarketResult.Ok,
+            error =>
+            {
+                var operationStatus = MapHttpStatusCodeToOperationStatus(response.StatusCode);
+                var errorMessage = getError(error);
+
+                return MarketResult.New(operationStatus, defaultValue, errorMessage);
+            }
+        );
+    }
+
+    private static MarketOperationStatus MapHttpStatusCodeToOperationStatus(HttpStatusCode code) =>
+        code switch
+        {
+            HttpStatusCode.BadRequest => MarketOperationStatus.BadRequest,
+            _ => MarketOperationStatus.UncaughtError
+        };
+}
