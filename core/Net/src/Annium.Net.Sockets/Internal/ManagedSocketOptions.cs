b0cbf40cf263ce713646ed5f4149460a98d29e17
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-11-08 22:42:12 +0300

    Net.Sockets: update

--- core/Net/src/Annium.Net.Sockets/Internal/ManagedSocketOptions.cs
+++ core/Net/src/Annium.Net.Sockets/Internal/ManagedSocketOptions.cs
@@ -2,6 +2,6 @@ namespace Annium.Net.Sockets.Internal;
 
 internal sealed record ManagedSocketOptions : ManagedSocketOptionsBase
 {
-    public static ManagedSocketOptions Default { get; } = new();
+    public new static ManagedSocketOptions Default { get; } = new();
     public SocketMode Mode { get; init; } = SocketMode.Raw;
 }
