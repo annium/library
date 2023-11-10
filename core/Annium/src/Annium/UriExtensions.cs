a3089a6bb4c7abe4c08b3c2b33fcceb88a002b99
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-11-10 18:35:44 +0300

    Annium: add UriExtensions.EnsureAbsolute

---
+++ core/Annium/src/Annium/UriExtensions.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Annium;
+
+public static class UriExtensions
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Uri EnsureAbsolute(this Uri uri, [CallerArgumentExpression("uri")] string uriEx = "")
+    {
+        if (!uri.IsAbsoluteUri)
+            throw new InvalidOperationException($"{uriEx} {uri} expected to be absolute uri");
+
+        return uri;
+    }
+}
