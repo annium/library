7e69dbf3fd21bddcb9a63192cd31c7d035eb46f7
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-11-28 12:17:24 +0300

    domain: update AssetDto / PositionDto - drop Id

--- finance/Providers/src/Annium.Finance.Providers.Abstractions.Domain/Dto/AssetDto.cs
+++ finance/Providers/src/Annium.Finance.Providers.Abstractions.Domain/Dto/AssetDto.cs
@@ -1,15 +1,3 @@
-using System;
-
 namespace Annium.Finance.Providers.Abstractions.Domain.Dto;
 
-public sealed record AssetDto(string Resource, decimal Free, decimal Locked)
-{
-    public Guid Id { get; private set; } = Guid.NewGuid();
-
-    public AssetDto SetId(Guid id)
-    {
-        Id = id;
-
-        return this;
-    }
-}
+public sealed record AssetDto(string Resource, decimal Free, decimal Locked);
--- finance/Providers/src/Annium.Finance.Providers.Abstractions.Domain/Dto/PositionDto.cs
+++ finance/Providers/src/Annium.Finance.Providers.Abstractions.Domain/Dto/PositionDto.cs
@@ -1,16 +1,10 @@
-using System;
 using Annium.Finance.Providers.Abstractions.Domain.Enums;
 
 namespace Annium.Finance.Providers.Abstractions.Domain.Dto;
 
-public sealed record PositionDto(string Symbol, OrientationRange OrientationRange, MarginType MarginType, byte Leverage)
-{
-    public Guid Id { get; private set; } = Guid.NewGuid();
-
-    public PositionDto SetId(Guid id)
-    {
-        Id = id;
-
-        return this;
-    }
-}
+public sealed record PositionDto(
+    string Symbol,
+    OrientationRange OrientationRange,
+    MarginType MarginType,
+    byte Leverage
+);
