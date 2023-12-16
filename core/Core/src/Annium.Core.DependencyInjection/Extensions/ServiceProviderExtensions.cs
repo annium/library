b3775d210c24e172a5dadb66b2f259349e8f0b34
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-12-17 01:25:29 +0300

    di: update ServiceProviderExtensions

--- core/Core/src/Annium.Core.DependencyInjection/Extensions/ServiceProviderExtensions.cs
+++ core/Core/src/Annium.Core.DependencyInjection/Extensions/ServiceProviderExtensions.cs
@@ -7,14 +7,48 @@ namespace Annium.Core.DependencyInjection;
 public static class ServiceProviderExtensions
 {
     public static T Resolve<T>(this IServiceProvider provider)
-        where T : notnull => provider.GetRequiredService<T>();
+        where T : notnull
+    {
+        return provider.GetRequiredService<T>();
+    }
 
-    public static object Resolve(this IServiceProvider provider, Type type) => provider.GetRequiredService(type);
+    public static T? TryResolve<T>(this IServiceProvider provider)
+        where T : notnull
+    {
+        return provider.GetService<T>();
+    }
 
-    public static T ResolveKeyed<TKey, T>(this IServiceProvider provider, TKey key)
-        where TKey : notnull
-        where T : notnull => provider.GetRequiredService<IIndex<TKey, T>>()[key];
+    public static object Resolve(this IServiceProvider provider, Type type)
+    {
+        return provider.GetRequiredService(type);
+    }
 
-    public static AsyncServiceScope CreateAsyncScope(this IServiceProvider provider) =>
-        new AsyncServiceScope(provider.CreateScope());
+    public static object? TryResolve(this IServiceProvider provider, Type type)
+    {
+        return provider.GetService(type);
+    }
+
+    public static T ResolveKeyed<T>(this IKeyedServiceProvider provider, object key)
+        where T : notnull
+    {
+        return (T)provider.GetRequiredKeyedService(typeof(T), key);
+    }
+
+    public static T? TryResolveKeyed<T>(this IKeyedServiceProvider provider, object key)
+        where T : notnull
+    {
+        var service = provider.GetKeyedService(typeof(T), key);
+
+        return service is not null ? (T)service : default;
+    }
+
+    public static object ResolveKeyed(this IKeyedServiceProvider provider, Type type, object key)
+    {
+        return provider.GetRequiredKeyedService(type, key);
+    }
+
+    public static object? TryResolveKeyed(this IKeyedServiceProvider provider, Type type, object key)
+    {
+        return provider.GetKeyedService(type, key);
+    }
 }
