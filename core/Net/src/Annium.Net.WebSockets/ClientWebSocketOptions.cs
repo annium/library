00ede623695e8c43e9f7a7b41ac625be3ee6be66
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-12-31 00:14:45 +0300

    Net.WebSockets: update ClientWebSocketOptions defaults

--- core/Net/src/Annium.Net.WebSockets/ClientWebSocketOptions.cs
+++ core/Net/src/Annium.Net.WebSockets/ClientWebSocketOptions.cs
@@ -4,7 +4,7 @@ public record ClientWebSocketOptions
 {
     public static ClientWebSocketOptions Default { get; } = new();
     public ConnectionMonitorOptions ConnectionMonitor { get; init; } = new();
-    public int KeepAliveInterval { get; init; } = 20_000;
-    public int ConnectTimeout { get; init; } = 3_000;
+    public int KeepAliveInterval { get; init; } = 3_000;
+    public int ConnectTimeout { get; init; } = 5_000;
     public int ReconnectDelay { get; init; }
 }
