4ea12823894475f8af036610aa180332dda35718
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-12-31 00:17:48 +0300

    binance: update ServerTimeWatcher confirmation interval

--- finance/Providers/src/Annium.Finance.Providers.Crypto.Binance.UsdFutures/ProviderConfiguration.cs
+++ finance/Providers/src/Annium.Finance.Providers.Crypto.Binance.UsdFutures/ProviderConfiguration.cs
@@ -6,7 +6,7 @@ namespace Annium.Finance.Providers.Crypto.Binance.UsdFutures;
 public sealed record ProviderConfiguration
 {
     public ListenKeyConfiguration ListenKey { get; init; } = new();
-    public ServerTimeWatcherConfig ServerTime { get; init; } = new(2_000, 3_600_000);
+    public ServerTimeWatcherConfig ServerTime { get; init; } = new(2_000, 5_000);
     public CompositeLoaderConfig ReloadAccount { get; init; } = new(3_000, 5, 5_000, 20_000, 3_000);
     public CompositeLoaderConfig ReloadOrders { get; init; } = new(3_000, 5, 10_000, 60_000, 5_000);
     public CompositeLoaderConfig ReloadTrades { get; init; } = new(5_000, 5, 10_000, 0, 5_000);
