a702769c4c36c8c65c641c68e2421fe03dd42c34
Alex Kreskiyan <a.kreskiyan@gmail.com>
2024-01-03 00:00:43 +0300

    Annium: add ObservableCollectionExtensions.ForceSort

--- core/Annium/src/Annium/Collections/ObjectModel/ObservableCollectionExtensions.cs
+++ core/Annium/src/Annium/Collections/ObjectModel/ObservableCollectionExtensions.cs
@@ -14,4 +14,14 @@ public static class ObservableCollectionExtensions
         for (var i = 0; i < sortableList.Count; i++)
             collection.Move(collection.IndexOf(sortableList[i]), i);
     }
+
+    public static void ForceSort<T>(this ObservableCollection<T> collection, Comparison<T> comparison)
+    {
+        var sortableList = new List<T>(collection);
+        sortableList.Sort(comparison);
+
+        collection.Clear();
+        foreach (var item in sortableList)
+            collection.Add(item);
+    }
 }
