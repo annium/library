d484477c52070e47d7ab37fbe8e5c16594c83249
Alex Kreskiyan <a.kreskiyan@gmail.com>
2024-01-06 15:20:11 +0300

    linq2db.Extensions: update DataOptions

--- integrations/linq2db/src/Annium.linq2db.Extensions/Configuration/DataOptionsExtensions.cs
+++ integrations/linq2db/src/Annium.linq2db.Extensions/Configuration/DataOptionsExtensions.cs
@@ -11,24 +11,26 @@ public static class DataOptionsExtensions
 {
     public static DataOptions UseLogging(this DataOptions options, IServiceProvider sp)
     {
-        var logSubject = sp.Resolve<ILogSubject>();
+        var logBridgeFactory = sp.Resolve<ILogBridgeFactory>();
 
         return options
             .UseTraceLevel(TraceLevel.Verbose)
             .UseTraceWith(
                 (msg, category, lvl) =>
                 {
+                    var logger = logBridgeFactory.Get(category ?? "linq2db");
+
                     switch (lvl)
                     {
                         case TraceLevel.Error:
-                            logSubject.Error<string?, string?>("{category}: {msg}", category, msg);
+                            logger.Error(msg ?? string.Empty);
                             break;
                         case TraceLevel.Warning:
-                            logSubject.Warn<string?, string?>("{category}: {msg}", category, msg);
+                            logger.Warn(msg ?? string.Empty);
                             break;
                         // all other levels are handled as Trace by design (at the moment linq2db traces with Info level)
                         default:
-                            logSubject.Trace<string?, string?>("{category}: {msg}", category, msg);
+                            logger.Trace(msg ?? string.Empty);
                             break;
                     }
                 }
