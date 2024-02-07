b7a5e318d5caf6b5bc3365c67be7b987c2514a37
Alex Kreskiyan <a.kreskiyan@gmail.com>
2024-02-07 14:08:34 +0300

    Annium: update Injected to allow null values and remove class constraint

--- core/Annium/src/Annium/Injected.cs
+++ core/Annium/src/Annium/Injected.cs
@@ -4,16 +4,25 @@ using System.Threading;
 namespace Annium;
 
 public sealed record Injected<T>
-    where T : class
 {
-    public T Value => _value.NotNull();
+    public T Value
+    {
+        get
+        {
+            if (_isInitialized == 0)
+                throw new InvalidOperationException($"{typeof(T).FriendlyName()} is not initialized");
+
+            return _value;
+        }
+    }
+
     private int _isInitialized;
-    private T? _value;
+    private T _value = default!;
 
     public void Init(T value)
     {
         if (Interlocked.CompareExchange(ref _isInitialized, 1, 0) != 0)
-            throw new InvalidOperationException("Already initialized");
+            throw new InvalidOperationException($"{typeof(T).FriendlyName()} is already initialized");
 
         _value = value;
     }
