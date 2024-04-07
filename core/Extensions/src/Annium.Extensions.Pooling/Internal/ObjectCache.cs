7747331b59fa0b7b2520edccde04ac24508d5a12
Alex Kreskiyan <a.kreskiyan@gmail.com>
2024-04-07 19:01:19 +0300

    base: add try/catch to ObjectCache with logging

--- core/Extensions/src/Annium.Extensions.Pooling/Internal/ObjectCache.cs
+++ core/Extensions/src/Annium.Extensions.Pooling/Internal/ObjectCache.cs
@@ -22,91 +22,113 @@ internal sealed class ObjectCache<TKey, TValue> : IObjectCache<TKey, TValue>, IL
 
     public async Task<IDisposableReference<TValue>> GetAsync(TKey key, CancellationToken ct = default)
     {
-        this.Trace("start");
-
-        // get or create CacheEntry
-        var ctx = new FactoryContext();
-        var entry = _entries.GetOrAdd(key, Factory, ctx);
-        var isNew = ReferenceEquals(ctx.Entry, entry);
-
-        var operation = isNew ? "new entry created" : "existing entry used";
-        this.Trace("Get by {key}: {operation} {entry}", key, operation, entry);
-
-        // creator - immediately creates value, others - wait for access
-        IDisposableReference<TValue>? reference = null;
-        if (isNew)
+        try
         {
-            this.Trace("Get by {key}: initialize entry {entry}", key, entry);
-            var value = await _provider.CreateAsync(key, ct);
-            value.Switch(
-                x => entry.SetValue(x),
-                x =>
-                {
-                    reference = x;
-                    entry.SetValue(x.Value);
-                }
-            );
+            this.Trace("start");
 
-            this.Trace("Get by {key}: entry {entry} ready", key, entry);
+            // get or create CacheEntry
+            var ctx = new FactoryContext();
+            var entry = _entries.GetOrAdd(key, Factory, ctx);
+            var isNew = ReferenceEquals(ctx.Entry, entry);
+
+            var operation = isNew ? "new entry created" : "existing entry used";
+            this.Trace("Get by {key}: {operation} {entry}", key, operation, entry);
+
+            // creator - immediately creates value, others - wait for access
+            IDisposableReference<TValue>? reference = null;
+            if (isNew)
+            {
+                this.Trace("Get by {key}: initialize entry {entry}", key, entry);
+                var value = await _provider.CreateAsync(key, ct);
+                value.Switch(
+                    x => entry.SetValue(x),
+                    x =>
+                    {
+                        reference = x;
+                        entry.SetValue(x.Value);
+                    }
+                );
+
+                this.Trace("Get by {key}: entry {entry} ready", key, entry);
+            }
+            else
+            {
+                this.Trace("Get by {key}: wait entry {entry}", key, entry);
+                await entry.WaitAsync();
+            }
+
+            // if not initializing and entry has no references - it is suspended, need to resume
+            if (!isNew && !entry.HasReferences)
+            {
+                this.Trace("Get by {key}: resume entry {entry}", key, entry);
+                await _provider.ResumeAsync(key, entry.Value);
+            }
+
+            // create reference, incrementing reference counter
+            this.Trace("Get by {key}: add entry {entry} reference", key, entry);
+            entry.AddReference();
+            reference ??= Disposable.Reference(entry.Value, () => Release(key, entry));
+
+            entry.Release();
+
+            return reference;
         }
-        else
+        catch (Exception e)
         {
-            this.Trace("Get by {key}: wait entry {entry}", key, entry);
-            await entry.WaitAsync();
+            this.Error(e);
+            throw;
         }
-
-        // if not initializing and entry has no references - it is suspended, need to resume
-        if (!isNew && !entry.HasReferences)
-        {
-            this.Trace("Get by {key}: resume entry {entry}", key, entry);
-            await _provider.ResumeAsync(key, entry.Value);
-        }
-
-        // create reference, incrementing reference counter
-        this.Trace("Get by {key}: add entry {entry} reference", key, entry);
-        entry.AddReference();
-        reference ??= Disposable.Reference(entry.Value, () => Release(key, entry));
-
-        entry.Release();
-
-        return reference;
     }
 
     private async Task Release(TKey key, CacheEntry entry)
     {
-        this.Trace("Release by {key}: wait entry {entry}", key, entry);
-        await entry.WaitAsync();
-
-        this.Trace("Release by {key}: remove reference from entry {entry}", key, entry);
-        entry.RemoveReference();
-        if (!entry.HasReferences)
+        try
         {
-            this.Trace("Release by {key}: suspend entry {entry}", key, entry);
-            await _provider.SuspendAsync(key, entry.Value);
-        }
+            this.Trace("Release by {key}: wait entry {entry}", key, entry);
+            await entry.WaitAsync();
 
-        entry.Release();
+            this.Trace("Release by {key}: remove reference from entry {entry}", key, entry);
+            entry.RemoveReference();
+            if (!entry.HasReferences)
+            {
+                this.Trace("Release by {key}: suspend entry {entry}", key, entry);
+                await _provider.SuspendAsync(key, entry.Value);
+            }
+
+            entry.Release();
+        }
+        catch (Exception e)
+        {
+            this.Error(e);
+        }
     }
 
     public async ValueTask DisposeAsync()
     {
-        this.Trace("start");
-
-        var cacheEntries = _entries.ToArray();
-        _entries.Clear();
-
-        this.Trace("dispose {count} entries", cacheEntries.Length);
-
-        foreach (var (key, entry) in cacheEntries)
+        try
         {
-            this.Trace("await {entry} value", entry);
-            await entry.WaitAsync();
-            this.Trace("dispose {entry}", entry);
-            entry.Dispose();
-            await _provider.DisposeAsync(key, entry.Value);
-        }
+            this.Trace("start");
 
-        this.Trace("done");
+            var cacheEntries = _entries.ToArray();
+            _entries.Clear();
+
+            this.Trace("dispose {count} entries", cacheEntries.Length);
+
+            foreach (var (key, entry) in cacheEntries)
+            {
+                this.Trace("await {entry} value", entry);
+                await entry.WaitAsync();
+                this.Trace("dispose {entry}", entry);
+                entry.Dispose();
+                await _provider.DisposeAsync(key, entry.Value);
+            }
+
+            this.Trace("done");
+        }
+        catch (Exception e)
+        {
+            this.Error(e);
+        }
     }
 
     private static CacheEntry Factory(TKey key, FactoryContext ctx) => ctx.Entry = new CacheEntry();
