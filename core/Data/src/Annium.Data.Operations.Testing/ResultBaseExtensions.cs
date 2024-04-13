1b0480d85a8b60e2158c9104abfadac3c6a85ddc
Alex Kreskiyan <a.kreskiyan@gmail.com>
2024-04-13 13:20:09 +0300

    Data.Operations.Testing: init

---
+++ core/Data/src/Annium.Data.Operations.Testing/ResultBaseExtensions.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Runtime.CompilerServices;
+using Annium.Testing;
+
+namespace Annium.Data.Operations.Testing;
+
+public static class ResultBaseExtensions
+{
+    public static void HasNoErrors(
+        this IResultBase value,
+        string? message = null,
+        [CallerArgumentExpression("value")] string valueEx = default!
+    )
+    {
+        if (value.HasErrors)
+            throw new AssertionFailedException(
+                message
+                    ?? $"{value.WrapWithExpression(valueEx)} contains errors: {Environment.NewLine}{value.ErrorState()}"
+            );
+    }
+
+    public static void HasErrors(
+        this IResultBase value,
+        string? message = null,
+        [CallerArgumentExpression("value")] string valueEx = default!
+    )
+    {
+        if (value.IsOk)
+            throw new AssertionFailedException(message ?? $"{value.WrapWithExpression(valueEx)} contains no errors");
+    }
+}
