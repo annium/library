4921be6cf88a65970b7e42fd3f533a8b0833c3c2
Alex Kreskiyan <a.kreskiyan@gmail.com>
2024-05-09 20:28:43 +0300

    Serialization.Abstractions: add ResolveSerializer overloads

--- core/Serialization/src/Annium.Serialization.Abstractions/ServiceProviderExtensions.cs
+++ core/Serialization/src/Annium.Serialization.Abstractions/ServiceProviderExtensions.cs
@@ -5,11 +5,24 @@ namespace Annium.Serialization.Abstractions;
 
 public static class ServiceProviderExtensions
 {
+    public static ISerializer<T> ResolveSerializer<T>(this IServiceProvider provider, string mediaType)
+    {
+        return provider.ResolveSerializer<T>(Constants.DefaultKey, mediaType);
+    }
+
     public static ISerializer<T> ResolveSerializer<T>(this IServiceProvider provider, string key, string mediaType)
     {
         var serializerKey = SerializerKey.Create(key, mediaType);
 
         return provider.GetRequiredKeyedService<ISerializer<T>>(serializerKey);
+    }
+
+    public static ISerializer<TSource, TDestination> ResolveSerializer<TSource, TDestination>(
+        this IServiceProvider provider,
+        string mediaType
+    )
+    {
+        return provider.ResolveSerializer<TSource, TDestination>(Constants.DefaultKey, mediaType);
     }
 
     public static ISerializer<TSource, TDestination> ResolveSerializer<TSource, TDestination>(
