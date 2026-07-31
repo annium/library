e87207deaf1b1ca04a09df07f7882a82e31038f4
Alex Kreskiyan <a.kreskiyan@gmail.com>
2026-07-31 12:22:02 +0300

    rename Backuper folder to xbackup

--- Backuper/src/Backuper.Api/Config/Configuration.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Backuper.Api.Config;
-
-public class Configuration
-{
-    public Dictionary<string, ServerConfiguration> Servers { get; set; } =
-        new Dictionary<string, ServerConfiguration>();
-}
