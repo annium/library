344a319b2daebbf06f3f705ba541aae3c26a37ae
Alexey Kreskiyan <a.kreskiyan@gmail.com>
2026-08-25 00:04:25 +0800

    chore: drop the obsolete Telegram demo host (#8)

--- integrations/Social/demo/Annium.Integrations.Social.Telegram.Demo.Obsolete/Program.cs
+++
@@ -1,7 +0,0 @@
-using System;
-using Annium.Core.Entrypoint;
-using Annium.Integrations.Social.Telegram.Demo.Obsolete;
-
-await using var entry = await Entrypoint.Default.UseServicePack<ServicePack>().SetupAsync();
-
-Console.WriteLine("Hello from Demo.Bots.Telegram");
